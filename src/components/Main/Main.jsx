a454c204c7187e9fc37297439c3a1181ff4fada9
TaeYoung Kim <taezero@twinny.ai>
2021-10-20 17:44:44 +0900

    Add: Main 영역 컴포넌트 (헤더 하단)

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,11 @@
+import CreateNote from '../CreateNote/CreateNote';
+import ReadAllNotes from '../ReadAllNotes/ReadAllNotes';
+import './Main.scss';
+const Main = () => (
+	<main>
+		<CreateNote />
+		<ReadAllNotes />
+	</main>
+);
+
+export default Main;
