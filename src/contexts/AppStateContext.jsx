ef8e3dc7acd9899da8ec624c990f192bc816e71e
TaeYoung Kim <taezero@twinny.ai>
2021-10-21 11:03:24 +0900

    Add: context api 도입으로 인한 context, provider 작성

---
+++ src/contexts/AppStateContext.jsx
@@ -0,0 +1,23 @@
+import { createContext, useContext } from 'react';
+import useCreateNote from '../hooks/useCreateNote';
+
+const AppStateContext = createContext();
+
+// State와 Action을 구분하여 사용하기 위함
+const useAppState = () => useContext(AppStateContext);
+const useAppAction = () => useContext(AppStateContext);
+
+// Provider
+const AppstateProvider = ({ children }) => {
+	// Combine
+	const combine = { ...useCreateNote() };
+
+	// Provider
+	return (
+		<AppStateContext.Provider value={combine}>
+			{children}
+		</AppStateContext.Provider>
+	);
+};
+
+export { AppstateProvider, useAppState, useAppAction };
