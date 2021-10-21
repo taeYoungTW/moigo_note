5c34317674b9acc7c69c74ee575840a473062bb1
TaeYoung Kim <taezero@twinny.ai>
2021-10-21 15:25:02 +0900

    Mod: SummaryNote 나열하여 연결

--- src/components/ReadAllNotes/ReadAllNotes.jsx
+++ src/components/ReadAllNotes/ReadAllNotes.jsx
@@ -1,7 +1,16 @@
+import { useAppState } from '../../contexts/AppStateContext';
+import SummaryNote from './SummaryNote';
 import './ReadAllNotes.scss';
 
 const ReadAllNotes = () => {
-	return <section>Area : Read all Notes</section>;
+	const { allNotes } = useAppState();
+	return (
+		<section className="summary_notes_ctnr">
+			{allNotes.map((note) => (
+				<SummaryNote note={note} key={note.id} />
+			))}
+		</section>
+	);
 };
 
 export default ReadAllNotes;
