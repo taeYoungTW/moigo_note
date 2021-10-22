6c3fb20673b02bd2a349fa12473ff0ba8efa8ca4
TaeYoung Kim <taezero@twinny.ai>
2021-10-22 14:19:10 +0900

    Mod: DetailNote 연결

--- src/components/ReadAllNotes/ReadAllNotes.jsx
+++ src/components/ReadAllNotes/ReadAllNotes.jsx
@@ -1,14 +1,16 @@
 import { useAppState } from '../../contexts/AppStateContext';
 import SummaryNote from './SummaryNote';
 import './ReadAllNotes.scss';
+import DetailNote from '../ReadNote/DetailNote';
 
 const ReadAllNotes = () => {
-	const { allNotes } = useAppState();
+	const { allNotes, detailNote } = useAppState();
 	return (
 		<section className="summary_notes_ctnr">
 			{allNotes.map((note) => (
 				<SummaryNote note={note} key={note.id} />
 			))}
+			{detailNote?.id && <DetailNote />}
 		</section>
 	);
 };
