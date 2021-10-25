745c1e7f169f1d3ab75b3c58fb76b3af5a55dbe6
TaeYoung Kim <taezero@twinny.ai>
2021-10-26 08:16:41 +0900

    Mod: proptypes 설정

--- src/components/Common/AddBtn.jsx
+++ src/components/Common/AddBtn.jsx
@@ -1,4 +1,5 @@
 import './AddBtn.scss';
+import PropTypes from 'prop-types';
 
 const AddBtn = ({ Icon, eventHandler }) => {
 	return (
@@ -9,6 +10,11 @@ const AddBtn = ({ Icon, eventHandler }) => {
 			/>
 		</button>
 	);
+};
+
+AddBtn.PropTyes = {
+	Icon: PropTypes.element,
+	// eventHandler: PropTypes.func.isRequired,
 };
 
 export default AddBtn;
