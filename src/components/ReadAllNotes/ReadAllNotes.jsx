c0eb5d87c2b852d8eab8953b65c7167ba32b1bfb
TaeYoung Kim <taezero@twinny.ai>
2021-10-25 14:25:31 +0900

    Mod: 삭제 Confirm 연결 구현

--- src/components/ReadAllNotes/ReadAllNotes.jsx
+++ src/components/ReadAllNotes/ReadAllNotes.jsx
@@ -1,16 +1,36 @@
-import { useAppState } from '../../contexts/AppStateContext';
+import { useAppAction, useAppState } from '../../contexts/AppStateContext';
 import SummaryNote from './SummaryNote';
 import './ReadAllNotes.scss';
 import DetailNote from '../ReadNote/DetailNote';
+import Confirm from '../Common/Confirm';
 
 const ReadAllNotes = () => {
-	const { allNotes, detailNote } = useAppState();
+	const { allNotes, detailNote, confirmNoteIdtoDelete } = useAppState();
+	const { deleteNote, setConfirmNoteIdtoDelete, offDetailNote } =
+		useAppAction();
+
+	function deleteCurrentNote() {
+		setConfirmNoteIdtoDelete('');
+		deleteNote(confirmNoteIdtoDelete);
+		offDetailNote();
+	}
+
 	return (
 		<section className="summary_notes_ctnr">
 			{allNotes.map((note) => (
 				<SummaryNote note={note} key={note.id} />
 			))}
 			{detailNote?.id && <DetailNote />}
+			<Confirm
+				isConfirmOn={confirmNoteIdtoDelete}
+				setIsConfirmOn={setConfirmNoteIdtoDelete}
+				question="선택한 노트를 삭제하시겠습니까?"
+				offConfirmBtnName="취소"
+			>
+				<button type="button" onClick={deleteCurrentNote}>
+					삭제
+				</button>
+			</Confirm>
 		</section>
 	);
 };
