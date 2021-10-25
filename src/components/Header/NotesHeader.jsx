9ca07567c17d8fe91575f64773e220eaa2d4b51f
TaeYoung Kim <taezero@twinny.ai>
2021-10-25 14:23:35 +0900

    Mod: 삭제 Confirm 모달 연결 구현

--- src/components/Header/NotesHeader.jsx
+++ src/components/Header/NotesHeader.jsx
@@ -2,9 +2,17 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import './NotesHeader.scss';
 import { useAppAction, useAppState } from '../../contexts/AppStateContext';
+import Confirm from '../Common/Confirm';
+import { useState } from 'react';
 const NotesHeader = () => {
 	const { selectedNoteIds } = useAppState();
 	const { deleteNotes, cancelSelect } = useAppAction();
+	const [isConfirmOn, setIsConfirmOn] = useState('');
+
+	function deleteSelectedNotes() {
+		deleteNotes(selectedNoteIds);
+		setIsConfirmOn('');
+	}
 	return (
 		<header className="note_header">
 			<div className="header_fixed">
@@ -16,10 +24,25 @@ const NotesHeader = () => {
 						{selectedNoteIds.length}개 선택됨
 					</h2>
 				</div>
-				<button className="delete_btn" onClick={deleteNotes}>
+				<button
+					className="delete_btn"
+					onClick={() => {
+						setIsConfirmOn('true');
+					}}
+				>
 					<DeleteIcon sx={{ fontSize: 19, color: '#767676' }} />
 				</button>
 			</div>
+			<Confirm
+				question={'선택한 노트를 삭제하시겠습니까?'}
+				offConfirmBtnName="취소"
+				isConfirmOn={isConfirmOn}
+				setIsConfirmOn={setIsConfirmOn}
+			>
+				<button type="button" onClick={deleteSelectedNotes}>
+					삭제
+				</button>
+			</Confirm>
 		</header>
 	);
 };
