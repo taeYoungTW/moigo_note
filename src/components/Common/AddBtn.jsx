cf323b7e541ceaa74cbc817a07477698f8844345
TaeYoung Kim <taezero@twinny.ai>
2021-10-26 14:48:10 +0900

    Mod: AddBtn onClickCapture에서 onClick으로 변경 (create_note_ctnr에서 stopPropagation)

--- src/components/Common/AddBtn.jsx
+++ src/components/Common/AddBtn.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 const AddBtn = ({ Icon, eventHandler }) => {
 	return (
-		<button className="add_btn" type="button" onClickCapture={eventHandler}>
+		<button className="add_btn" type="button" onClick={eventHandler}>
 			<Icon
 				sx={{ fontSize: 22, cursor: 'pointer', color: '#2A394B' }}
 				className="addBtn_icon"
@@ -14,7 +14,7 @@ const AddBtn = ({ Icon, eventHandler }) => {
 
 AddBtn.PropTyes = {
 	Icon: PropTypes.element,
-	// eventHandler: PropTypes.func.isRequired,
+	eventHandler: PropTypes.func,
 };
 
 export default AddBtn;
