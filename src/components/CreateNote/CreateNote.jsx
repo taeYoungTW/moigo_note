4be5a453ee2fae3771464d6c6a29f7f633537606
TaeYoung Kim <taezero@twinny.ai>
2021-10-26 14:53:23 +0900

    Mod: CreateNote 상태에서 블록 추가 버튼 기능 구현 (AddTextBlock, AddChecklistBlock)

--- src/components/CreateNote/CreateNote.jsx
+++ src/components/CreateNote/CreateNote.jsx
@@ -4,14 +4,28 @@ import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
 import AddBtn from '../Common/AddBtn';
 import CreateNoteForm from './CreateNoteForm';
 import { useAppAction, useAppState } from '../../contexts/AppStateContext';
+import { v4 as uuid } from 'uuid';
 
 const CreateNote = () => {
 	const { isOnCreateNote } = useAppState();
-	const { changeIsOnCreateNote } = useAppAction();
-	function handleAdd(e) {
-		e.preventDefault();
+	const { changeIsOnCreateNote, addBlock } = useAppAction();
+
+	function AddTextBlock(e) {
+		// e.stopPropagation();
 		changeIsOnCreateNote(true);
+		addBlock({ id: uuid(), type: 'text', text: '' });
 	}
+
+	function AddChecklistBlock(e) {
+		changeIsOnCreateNote(true);
+		addBlock({ id: uuid(), type: 'checklist', isDone: false, content: '' });
+	}
+
+	function AddImageBlock(e) {
+		changeIsOnCreateNote(true);
+		addBlock({ id: uuid(), type: 'image', baseURL: '' });
+	}
+
 	return (
 		<section className="create_note">
 			{isOnCreateNote ? (
@@ -23,11 +37,14 @@ const CreateNote = () => {
 						e.stopPropagation();
 					}}
 				>
-					<button className="add_text_btn" onClick={handleAdd}>
+					<button className="add_text_btn" onClick={AddTextBlock}>
 						노트 작성...
 					</button>
-					<AddBtn Icon={InsertPhotoIcon} eventHandler={handleAdd} />
-					<AddBtn Icon={FormatListBulletedIcon} eventHandler={handleAdd} />
+					<AddBtn Icon={InsertPhotoIcon} eventHandler={AddImageBlock} />
+					<AddBtn
+						Icon={FormatListBulletedIcon}
+						eventHandler={AddChecklistBlock}
+					/>
 				</div>
 			)}
 		</section>
