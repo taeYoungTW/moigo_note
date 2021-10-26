fe8e170e0452dd9b34532da30455c0f08243f4ed
TaeYoung Kim <taezero@twinny.ai>
2021-10-26 14:58:11 +0900

    Mod: createNoteForm 상태에서 block이 없는 경우에만 외부 영역 클릭시 createNoteForm 닫기 구현 (blocks 초기화)

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,15 +1,19 @@
-import { useAppAction } from '../../contexts/AppStateContext';
+import { useAppAction, useAppState } from '../../contexts/AppStateContext';
 import CreateNote from '../CreateNote/CreateNote';
 import ReadAllNotes from '../ReadAllNotes/ReadAllNotes';
 import './Main.scss';
 const Main = () => {
-	const { changeIsOnCreateNote } = useAppAction();
+	const { changeIsOnCreateNote, resetBlocks } = useAppAction();
+	const { blocks } = useAppState();
 
 	// SummaryNote 또는 외부 영역 클릭시, CreateNoteForm -> CreateNote 상태로 변경되게 함 (이벤트 버블링 사용)
 	return (
 		<main
 			onClick={() => {
-				changeIsOnCreateNote(false);
+				if (blocks.length === 0) {
+					resetBlocks();
+					changeIsOnCreateNote(false);
+				}
 			}}
 		>
 			<CreateNote />
