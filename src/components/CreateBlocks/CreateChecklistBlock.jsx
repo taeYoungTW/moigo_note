5e5d26e8d793aedc016306734396fb267346f434
TaeYoung Kim <taezero@twinny.ai>
2021-10-27 16:59:02 +0900

    Mod: localState 삭제 및 block prop으로 대체, isUpdate prop을 통해 input, label id 중복 제거

--- src/components/CreateBlocks/CreateChecklistBlock.jsx
+++ src/components/CreateBlocks/CreateChecklistBlock.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef } from 'react';
 import DeleteIcon from '@mui/icons-material/Delete';
 import MenuIcon from '@mui/icons-material/Menu';
 import './CreateBlocks.scss';
@@ -6,27 +6,28 @@ import { useAppAction } from '../../contexts/AppStateContext';
 import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
 import CheckBoxIcon from '@mui/icons-material/CheckBox';
 
-const CreateChecklistBlock = ({ block }) => {
+const CreateChecklistBlock = ({ block, isUpdate }) => {
 	// Global State, Actions
 	const { deleteBlock, updateBlock } = useAppAction();
 
 	// Local State
-	const [checklistBlock, setChecklistBlock] = useState(block);
 	const contentRef = useRef(null);
 
 	// Functions
-
 	// useEffect : textarea auto height
 	useEffect(() => {
 		contentRef.current.style.height = '';
 		contentRef.current.style.height = contentRef.current.scrollHeight + 'px';
-	}, [checklistBlock]);
+	}, [block]);
 
 	return (
 		<div className="create_block">
 			<div className="checklist">
-				<label htmlFor={block.id} className="checkbox_label">
-					{checklistBlock.isDone ? (
+				<label
+					htmlFor={isUpdate ? `updateNote_${block.id}` : block.id}
+					className="checkbox_label"
+				>
+					{block.isDone ? (
 						<CheckBoxIcon sx={{ fontSize: 20, color: '#29394B' }} />
 					) : (
 						<CheckBoxOutlineBlankIcon sx={{ fontSize: 20, color: '#29394B' }} />
@@ -34,26 +35,25 @@ const CreateChecklistBlock = ({ block }) => {
 				</label>
 				<input
 					type="checkbox"
-					id={block.id}
+					id={isUpdate ? `updateNote_${block.id}` : block.id}
 					className="checkbox"
-					onClick={(e) => {
+					checked={block.isDone}
+					onChange={(e) => {
 						const {
 							target: { checked },
 						} = e;
-						updateBlock({ ...checklistBlock, isDone: checked });
-						setChecklistBlock((block) => ({ ...block, isDone: checked }));
+						updateBlock({ ...block, isDone: checked });
 					}}
 				/>
 				<textarea
 					className="checklist_block_textarea"
 					type="text"
-					value={checklistBlock.content}
+					value={block.content}
 					onChange={(e) => {
 						const {
 							target: { value },
 						} = e;
-						updateBlock({ ...checklistBlock, content: value });
-						setChecklistBlock((block) => ({ ...block, content: value }));
+						updateBlock({ ...block, content: value });
 					}}
 					placeholder="항목 추가"
 					rows={1}
@@ -61,8 +61,7 @@ const CreateChecklistBlock = ({ block }) => {
 					spellCheck={false}
 					autoFocus={true}
 					style={{
-						textDecoration:
-							checklistBlock.isDone && 'solid line-through #767676 1px',
+						textDecoration: block.isDone && 'solid line-through #767676 1px',
 					}}
 				/>
 			</div>
