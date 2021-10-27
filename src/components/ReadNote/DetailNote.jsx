27f961f5c0d14f9552ad3ef5c664ebf3fc42df7c
TaeYoung Kim <taezero@twinny.ai>
2021-10-27 17:12:40 +0900

    Mod: DetailNote content 영역 block 표시하기 구현

--- src/components/ReadNote/DetailNote.jsx
+++ src/components/ReadNote/DetailNote.jsx
@@ -4,12 +4,12 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import { useAppAction, useAppState } from '../../contexts/AppStateContext';
 import { useState } from 'react';
 import UpdateNote from '../UpdateNote/UpdateNote';
+import ReadChecklistBlock from '../ReadBlocks/ReadChecklistBlock';
+import ReadTextBlock from '../ReadBlocks/ReadTextBlock';
 const DetailNote = () => {
-	const {
-		detailNote: { title, id },
-	} = useAppState();
-	const { offDetailNote, setConfirmNoteIdtoDelete } = useAppAction();
+	const { detailNote } = useAppState();
 
+	const { offDetailNote, setConfirmNoteIdtoDelete } = useAppAction();
 	const [isEdit, setIsEdit] = useState(false);
 
 	return isEdit ? (
@@ -18,17 +18,42 @@ const DetailNote = () => {
 		<div className="detail_note_ctnr">
 			<div className="detail_note">
 				<div className="title_ctnr">
-					<h1 className="title">{title}</h1>
+					<h1 className="title">{detailNote.title}</h1>
 					<button className="close_btn" onClick={offDetailNote}>
 						<CloseIcon sx={{ fontSize: 25, color: '#767676' }} />
 					</button>
 				</div>
-				<div className="content">Area for Block</div>
+				<div className="detailNote_content">
+					{detailNote.blocks &&
+						detailNote.blocks.map((block) => {
+							switch (block.type) {
+								case 'text':
+									return (
+										<ReadTextBlock
+											block={block}
+											key={block.id}
+											isDetail={true}
+										/>
+									);
+								case 'checklist':
+									return (
+										<ReadChecklistBlock
+											block={block}
+											key={`DetailNote_${block.id}`}
+											noteId={detailNote.id}
+											isDetail={true}
+										/>
+									);
+								default:
+									return new Error('Error: Read Block');
+							}
+						})}
+				</div>
 				<div className="ctrl_bar">
 					<button
 						className="delete_btn"
 						onClick={() => {
-							setConfirmNoteIdtoDelete(id);
+							setConfirmNoteIdtoDelete(detailNote.id);
 						}}
 					>
 						<DeleteIcon sx={{ fontSize: 23, color: '#2a394b' }} />
