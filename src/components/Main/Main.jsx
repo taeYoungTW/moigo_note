8ea16df772f9b430d81c6af30119cc919294d86a
TaeYoung Kim <taezero@twinny.ai>
2021-10-27 17:06:33 +0900

    Mod: 외부 영역 클릭시 CreateNoteForm -> CreateNote 상태 변경 조건 상세화 (블록 개수가 1개 이면서, 첫번째 블럭의 text가 없는 경우)

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,15 +3,15 @@ import CreateNote from '../CreateNote/CreateNote';
 import ReadAllNotes from '../ReadAllNotes/ReadAllNotes';
 import './Main.scss';
 const Main = () => {
-	const { changeIsOnCreateNote, resetBlocks } = useAppAction();
+	const { changeIsOnCreateNote } = useAppAction();
 	const { blocks } = useAppState();
 
 	// SummaryNote 또는 외부 영역 클릭시, CreateNoteForm -> CreateNote 상태로 변경되게 함 (이벤트 버블링 사용)
 	return (
 		<main
 			onClick={() => {
-				if (blocks.length === 0) {
-					resetBlocks();
+				const firstBlock = blocks[0];
+				if (blocks.length === 1 && !firstBlock.text) {
 					changeIsOnCreateNote(false);
 				}
 			}}
