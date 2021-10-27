7e632cd2c481ccd717418cce69cf5b08793d0913
TaeYoung Kim <taezero@twinny.ai>
2021-10-27 17:14:21 +0900

    Mod: UpdateNote Content 영역 Block 표시 구현, default block 기능 구현, unmount 시 global state blocks 초기화 구현

--- src/components/UpdateNote/UpdateNote.jsx
+++ src/components/UpdateNote/UpdateNote.jsx
@@ -5,16 +5,48 @@ import InsertPhotoIcon from '@mui/icons-material/InsertPhoto';
 import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
 import TextFieldsIcon from '@mui/icons-material/TextFields';
 import './UpdateNote.scss';
-import { useState } from 'react';
-const UpdateNote = () => {
-	const { detailNote } = useAppState();
-	const { offDetailNote, updateNote } = useAppAction();
+import { useEffect, useState } from 'react';
+import CreateTextBlock from '../CreateBlocks/CreateTextBlock';
+import CreateChecklistBlock from '../CreateBlocks/CreateChecklistBlock';
+import { v4 as uuid } from 'uuid';
 
+const UpdateNote = () => {
+	const { blocks, detailNote } = useAppState();
+	const { offDetailNote, updateNote, addBlock, initBlocks, resetBlocks } =
+		useAppAction();
 	const [note, setNote] = useState(detailNote);
+
 	function updateNoteSubmit(e) {
 		e.preventDefault();
-		updateNote(note);
+		updateNote({ ...note, blocks: [...blocks] });
 	}
+
+	function addTextBlock(e) {
+		addBlock({ id: uuid(), type: 'text', text: '' });
+	}
+
+	function addChecklistBlock(e) {
+		addBlock({ id: uuid(), type: 'checklist', isDone: false, content: '' });
+	}
+
+	/* 	function addImageBlock(e) {
+		addBlock({ id: uuid(), type: 'image', baseURL: '' });
+	} */
+
+	useEffect(() => {
+		if (blocks.length === 0) {
+			addTextBlock();
+		}
+	}, [blocks.length]);
+
+	useEffect(() => {
+		initBlocks([...detailNote.blocks]);
+
+		return () => {
+			resetBlocks();
+		};
+	}, [detailNote, initBlocks, resetBlocks]);
+
 	return (
 		<div className="update_note_ctnr">
 			<div className="update_note">
@@ -32,12 +64,37 @@ const UpdateNote = () => {
 						<CloseIcon sx={{ fontSize: 25, color: '#767676' }} />
 					</button>
 				</div>
-				<div className="content">Area for Block</div>
+				<div className="content">
+					{blocks.map((block) => {
+						switch (block.type) {
+							case 'text':
+								return (
+									<CreateTextBlock
+										block={block}
+										key={`updateNote_${block.id}`}
+									/>
+								);
+							case 'checklist':
+								return (
+									<CreateChecklistBlock
+										block={block}
+										key={`updateNote_${block.id}`}
+										isUpdate={true}
+									/>
+								);
+							default:
+								return '';
+						}
+					})}
+				</div>
 				<div className="ctrl_bar">
 					<div className="add_btns">
 						<AddBtn Icon={InsertPhotoIcon} />
-						<AddBtn Icon={FormatListBulletedIcon} />
-						<AddBtn Icon={TextFieldsIcon} />
+						<AddBtn
+							Icon={FormatListBulletedIcon}
+							eventHandler={addChecklistBlock}
+						/>
+						<AddBtn Icon={TextFieldsIcon} eventHandler={addTextBlock} />
 					</div>
 					<button
 						type="button"
