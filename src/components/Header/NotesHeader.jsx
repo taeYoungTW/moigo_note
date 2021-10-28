72935fdbae5d065f5080b2051dba73451787d0cc
TaeYoung Kim <taezero@twinny.ai>
2021-10-28 12:19:38 +0900

    Mod: selectedNoteIds, deleteNotes,resetSelectedNoteIds -> global states, actions 식별자 _ 형태로 변경

--- src/components/Header/NotesHeader.jsx
+++ src/components/Header/NotesHeader.jsx
@@ -5,23 +5,23 @@ import { useAppAction, useAppState } from '../../contexts/AppStateContext';
 import Confirm from '../Common/Confirm';
 import { useState } from 'react';
 const NotesHeader = () => {
-	const { selectedNoteIds } = useAppState();
-	const { deleteNotes, resetSelectedNoteIds } = useAppAction();
+	const { _selectedNoteIds } = useAppState();
+	const { _deleteNotes, _resetSelectedNoteIds } = useAppAction();
 	const [isConfirmOn, setIsConfirmOn] = useState('');
 
 	function deleteSelectedNotes() {
-		deleteNotes(selectedNoteIds);
+		_deleteNotes(_selectedNoteIds);
 		setIsConfirmOn('');
 	}
 	return (
 		<header className="note_header">
 			<div className="header_fixed">
 				<div className="align_left">
-					<button className="cancel_btn" onClick={resetSelectedNoteIds}>
+					<button className="cancel_btn" onClick={_resetSelectedNoteIds}>
 						<ArrowBackIcon sx={{ fontSize: 19, color: '#767676' }} />
 					</button>
 					<h2 className="selected_note_count">
-						{selectedNoteIds.length}개 선택됨
+						{_selectedNoteIds.length}개 선택됨
 					</h2>
 				</div>
 				<button
