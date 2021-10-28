4c82408400f833cce0ef977f21bde6fcbab31994
TaeYoung Kim <taezero@twinny.ai>
2021-10-28 15:11:05 +0900

    Mod: Actions, State 식별자 형식 _ 형태로 통일 및 주석 설명 추가, _deleteNote 속도 개선, Actions 독립성 강화

--- src/hooks/useNote.jsx
+++ src/hooks/useNote.jsx
@@ -5,65 +5,91 @@ const useNote = () => {
 	/*
 	 * ------ Init State : useState ------------------
 	 */
-	const [isOnCreateNoteForm, setIsOnCreateNoteForm] = useState(false); // ex. true or false
-	const [selectedNoteIds, setSelectedNoteIds] = useState([]); // ex. [ id, id, id, ...]
-	const [allNotes, setAllNotes] = useState([]); // ex. [{title, id, blocks}, {title, id, blocks}, {title, id, blocks}, ... ]
-	const [detailNote, setDetailNote] = useState({}); // ex. {title, id, blocks}
-	const [confirmNoteIdtoDelete, setConfirmNoteIdtoDelete] = useState(''); // string : Put a specific Note id to delete
+	const [_isOnCreateNoteForm, setIsOnCreateNoteForm] = useState(false); // ex. true or false
+	const [_selectedNoteIds, setSelectedNoteIds] = useState([]); // ex. [ id, id, id, ...]
+	const [_allNotes, setAllNotes] = useState([]); // ex. [{title, id, blocks}, {title, id, blocks}, {title, id, blocks}, ... ]
+	const [_detailNote, setDetailNote] = useState({}); // ex. {title, id, blocks}
+	const [_confirmNoteIdToDelete, setConfirmNoteIdToDelete] = useState(''); // string : Put a specific Note id to delete
+	/* 
+	_conFirmNoteIdToDelete의 필요성
+		SummaryNote에서 noteId를 줄 수 있지만, Confirm을 표시할 스타일이 SummaryNote의 position relative에 막혀 absolute로 전체 화면을 감쌀 수 없기 때문에
+		Confirm 위치를 ReadAllNotes에 둘 수 밖에 없고 해당 위치에서 삭제 구현을 위해 특정 noteId를 필요로 함으로 confirmNoteIdToDelete를 사용하였습니다.
+	*/
 
 	/*
 	 * ------ Actions: useCallback -------------------
 	 */
+
+	/*
+	 * ~~~~ About Confirm ~~~~
+	 * - _setConfirmNoteIdToDelete : noteId or '' (For using Confirm Component Callback)
+	 */
+	const _setConfirmNoteIdToDelete = useCallback((noteId) => {
+		setConfirmNoteIdToDelete(noteId);
+	}, []);
 	/*
 	 * ~~~~ About State of CreateNote Area ~~~~
-	 * - changeIsOnCreateNoteForm : Change "isOn(focus)"
+	 * - _changeIsOnCreateNoteForm : Change "isOn(focus)"
 	 * true : (CreateNote -> CreateNoteForm)
 	 * 									Or
 	 * false : (CreateNoteForm -> CreateNote)
 	 */
-	const changeIsOnCreateNoteForm = useCallback((value) => {
+	const _changeIsOnCreateNoteForm = useCallback((value) => {
 		setIsOnCreateNoteForm(value);
 	}, []);
 	/**
 	 * ~~~~ About Selection ~~~~
-	 *	- addSelectedNotedId : add A selected NoteID To SelectedNoteIds
-	 *	- deleteSelectedNoteId : delete A selected NoteID To SelectedNoteIds
-	 *	- resetSelectedNoteIds : reset SelectedNoteIds
+	 *	- _addSelectedNotedId : add A selected NoteID To SelectedNoteIds
+	 *	- _deleteSelectedNoteId : delete A selected NoteID from SelectedNoteIds
+	 *	- _resetSelectedNoteIds : reset SelectedNoteIds
+	 *	- _deleteSelectedNoteIds : delete selected IDs from SelectedNoteIds
 	 */
-	const addSelectedNoteId = useCallback((id) => {
+	const _addSelectedNoteId = useCallback((id) => {
 		setSelectedNoteIds((ids) => [...ids, id]);
 	}, []);
 
-	const deleteSelectedNoteId = useCallback(
+	const _deleteSelectedNoteId = useCallback(
 		(id) => {
-			const newSelectedNoteIds = selectedNoteIds.filter(
+			const newSelectedNoteIds = _selectedNoteIds.filter(
 				(noteId) => noteId !== id
 			);
 			setSelectedNoteIds(newSelectedNoteIds);
 		},
-		[selectedNoteIds]
+		[_selectedNoteIds]
 	);
 
-	const resetSelectedNoteIds = useCallback(() => {
+	const _resetSelectedNoteIds = useCallback(() => {
 		setSelectedNoteIds([]);
 	}, []);
 
+	/* 
+	const _deleteSelectedNoteIds = useCallback((inputIds) => {
+		let tempIds = _selectedNoteIds;
+
+		tempIds = inputIds.reduce((acc, inputId)=>{
+			return acc.filter((id)=> id !== inputId)
+		},tempIds)
+
+			setSelectedNoteIds(tempIds);
+	}, [_selectedNoteIds]); 
+	*/
+
 	/*
 	 * ~~~~ About Detail Note (ON/OFF) ~~~~
-	 * - onDetailNote : Add a specific Note to detailNote (Global State) when user click the specific SummaryNote.
+	 * - _setDetailNote : Add a specific Note to _detailNote (Global State) when user click the specific SummaryNote.
 	 * 									After that, DetailNot Component is rendered
-	 * - offDetailNote : Reset detailNote state
-	 * - updateDetailNoteChecklist : Update A specific ChecklistBlock for detailNote state
+	 * - _resetDetailNote : Reset _detailNote state
+	 * - _updateChecklistOfDetailNote : Update A specific ChecklistBlock of _detailNote state
 	 */
-	const onDetailNote = useCallback((note) => {
+	const _setDetailNote = useCallback((note) => {
 		setDetailNote(note);
 	}, []);
 
-	const offDetailNote = useCallback(() => {
+	const _resetDetailNote = useCallback(() => {
 		setDetailNote({});
 	}, []);
 
-	const updateDetailNoteChecklist = useCallback(
+	const _updateChecklistOfDetailNote = useCallback(
 		(targetBlock) =>
 			setDetailNote((detailNote) => {
 				const newBlocks = detailNote.blocks.map((block) => {
@@ -80,54 +106,69 @@ const useNote = () => {
 	);
 
 	/*
-	 * ~~~~ About "allNotes" state ~~~~
-	 * - addNote : Add a specific Note to "allNotes" state
-	 * - deleteNote : Delete a specific Note to "allNotes" state
-	 * - deleteNotes : Delete specific Notes (selected Notes) to "allNotes" state
-	 * - updateNote : Update a specific Note of "allNotes" state
-	 * - updateNoteChecklist : Update a specific ChecklistBlock of a specific Note (so, need noteId and block)
+	 * ~~~~ About "_allNotes" state ~~~~
+	 * - _addNote : Add a specific Note to "_allNotes" state
+	 * - _deleteNote : Delete a specific Note from "_allNotes" state
+	 * - _deleteNotes : Delete specific Notes (selected Notes) from "_allNotes" state
+	 * - _updateNote : Update a specific Note of "_allNotes" state
+	 * - _updateChecklistOfNote : Update a specific ChecklistBlock of a specific Note (so, need noteId and block)
 	 */
-	const addNote = useCallback(
+	const _addNote = useCallback(
 		(note) => {
-			allNotes.unshift(note);
-			setAllNotes([...allNotes]);
+			_allNotes.unshift(note);
+			setAllNotes([..._allNotes]);
 		},
-		[allNotes]
+		[_allNotes]
 	);
 
-	const deleteNote = useCallback(
+	const _deleteNote = useCallback(
 		(id) => {
-			const newAllNotes = allNotes.filter((note) => note.id !== id);
+			const newAllNotes = _allNotes.filter((note) => note.id !== id);
 			setAllNotes(newAllNotes);
-
-			const isSelected = selectedNoteIds.includes(id);
-			if (isSelected) {
-				deleteSelectedNoteId(id);
-			}
 		},
-		[allNotes, selectedNoteIds, deleteSelectedNoteId]
+		[_allNotes]
 	);
 
-	const deleteNotes = useCallback(() => {
-		let tempAllNotes = allNotes;
-		selectedNoteIds.forEach((id) => {
-			tempAllNotes = tempAllNotes.filter((note) => note.id !== id);
-		});
-		setSelectedNoteIds([]); // Cancel Selected Notes
-		setAllNotes(tempAllNotes);
-	}, [allNotes, selectedNoteIds]);
+	const _deleteNotes = useCallback(
+		(inputIds) => {
+			let tempAllNotes = _allNotes;
+			for (let i = 0; i < inputIds.length; i++) {
+				for (let j = 0; j < tempAllNotes.length; j++) {
+					if (inputIds[i] === tempAllNotes[j].id) {
+						tempAllNotes.splice(j, 1);
+						break;
+					}
+				}
+			}
+			setAllNotes([...tempAllNotes]);
+		},
+		[_allNotes]
+	); // reduce 방식과 비교했을 때 작은 숫자의 경우 큰 차이는 없으나, 10만 단위 이상시 차이가 발생했습니다.
 
-	const updateNote = useCallback(
+	/* 
+	const _deleteNotesReduce = useCallback(
+		(inputIds) => {
+			const tempAllNotes = inputIds.reduce((acc, inputId) => {
+				return acc.filter((note) => note.id !== inputId);
+			}, _allNotes);
+
+			setAllNotes(tempAllNotes);
+		},
+		[_allNotes]
+	); 
+	*/
+
+	const _updateNote = useCallback(
 		(newNote) => {
-			const newAllNotes = allNotes.filter((note) => note.id !== newNote.id);
+			const newAllNotes = _allNotes.filter((note) => note.id !== newNote.id);
 			newAllNotes.unshift(newNote);
 			setAllNotes(newAllNotes);
 			setDetailNote({});
 		},
-		[allNotes]
+		[_allNotes]
 	);
 
-	const updateNoteChecklist = useCallback((noteId, targetBlock) => {
+	const _updateChecklistOfNote = useCallback((noteId, targetBlock) => {
 		setAllNotes((AllNotes) => {
 			return AllNotes.map((note) => {
 				if (note.id === noteId) {
@@ -146,26 +187,26 @@ const useNote = () => {
 
 	// ------ Combine States & Actions ------
 	const combineStates = {
-		allNotes,
-		isOnCreateNoteForm,
-		selectedNoteIds,
-		detailNote,
-		confirmNoteIdtoDelete,
+		_allNotes,
+		_isOnCreateNoteForm,
+		_selectedNoteIds,
+		_detailNote,
+		_confirmNoteIdToDelete,
 	};
 	const combineActions = {
-		addNote,
-		changeIsOnCreateNoteForm,
-		deleteNote,
-		deleteNotes,
-		addSelectedNoteId,
-		deleteSelectedNoteId,
-		resetSelectedNoteIds,
-		onDetailNote,
-		offDetailNote,
-		updateNote,
-		setConfirmNoteIdtoDelete,
-		updateNoteChecklist,
-		updateDetailNoteChecklist,
+		_addNote,
+		_changeIsOnCreateNoteForm,
+		_deleteNote,
+		_deleteNotes,
+		_addSelectedNoteId,
+		_deleteSelectedNoteId,
+		_resetSelectedNoteIds,
+		_setDetailNote,
+		_resetDetailNote,
+		_updateNote,
+		_setConfirmNoteIdToDelete,
+		_updateChecklistOfNote,
+		_updateChecklistOfDetailNote,
 	};
 
 	return { ...combineStates, ...combineActions };
