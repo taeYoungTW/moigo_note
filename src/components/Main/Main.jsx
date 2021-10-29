9b5549b51f1277e93c5d900233f072e876e45aa4
TaeYoung Kim <taezero@twinny.ai>
2021-10-29 09:54:30 +0900

    Mod: handleMainOnClick에 CreateNoteForm을 off로 하는 조건 상세화 (checklist 빈값도 포함)

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -13,10 +13,14 @@ const Main = () => {
 	 * SummaryNote 또는 외부 영역 클릭시,
 	 * CreateNoteForm -> CreateNote 상태로 변경되게 합니다.
 	 * (이벤트 버블링 사용하여 CreateNoteForm Component Off)
+	 * 조건: 블록의 개수가 1개 이면서, 해당 블럭의 text 또는 content의 내용이 없는 경우
 	 */
 	const handleMainOnClick = useCallback(() => {
 		const firstBlock = _blocks[0];
-		if (_blocks.length === 1 && !firstBlock.text) {
+		if (
+			_blocks.length === 1 &&
+			(firstBlock.text === '' || firstBlock.content === '')
+		) {
 			_changeIsOnCreateNoteForm(false);
 		}
 	}, [_blocks, _changeIsOnCreateNoteForm]);
