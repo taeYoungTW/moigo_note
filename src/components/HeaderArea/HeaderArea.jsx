f4939d7d842173ae906883160f09ea003fb2dad5
TaeYoung Kim <taezero@twinny.ai>
2021-11-01 15:29:48 +0900

    Add: 상태 값에 따라 Header, NotesHeader를 연결해주는 컴포넌트 생성

---
+++ src/components/HeaderArea/HeaderArea.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useAppState } from '../../contexts/AppStateContext';
+import Header from '../Header/Header';
+import NotesHeader from '../NotesHeader/NotesHeader';
+import './HeaderArea.scss';
+
+const HeaderArea = () => {
+	// Global States, Actions ---------------------------------------
+	const { _selectedNoteIds } = useAppState();
+
+	// Render -------------------------------------------------------
+	return (
+		<header className={_selectedNoteIds.length === 0 ? '' : 'note_header'}>
+			<div className="header_fixed">
+				{_selectedNoteIds.length === 0 ? <Header /> : <NotesHeader />}
+			</div>
+		</header>
+	);
+};
+
+export default HeaderArea;
