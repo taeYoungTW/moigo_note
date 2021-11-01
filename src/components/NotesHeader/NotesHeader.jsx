226ed04c87836b3b2ba5d60f3f5ea57b699991d7
TaeYoung Kim <taezero@twinny.ai>
2021-11-01 15:34:05 +0900

    Mod: 상수 독립 관리로 인한 상수 식별자 연결

---
+++ src/components/NotesHeader/NotesHeader.jsx
@@ -0,0 +1,70 @@
+import DeleteIcon from '@mui/icons-material/Delete';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import './NotesHeader.scss';
+import { useAppAction, useAppState } from '../../contexts/AppStateContext';
+import { useCallback, useState } from 'react';
+import PortalConfirm from '../Common/PortalConfirm';
+import {
+	COUNT_OF_SELECTED_NOTE_TEXT,
+	DO_YOU_WANT_TO_DELETE_SLECTED_NOTES_TEXT,
+	HEADER_ARROWBACK_ICON_COLOR,
+	HEADER_ARROWBACK_ICON_FONT_SIZE,
+	HEADER_DELETE_ICON_COLOR,
+	HEADER_DELETE_ICON_FONT_SIZE,
+} from '../../constants/constants';
+
+const NotesHeader = () => {
+	// Global States, Actions ---------------------------------------
+	const { _selectedNoteIds } = useAppState();
+	const { _deleteNotes, _resetSelectedNoteIds } = useAppAction();
+
+	// Local States ------------------------------------------------
+	const [isConfirmOn, setIsConfirmOn] = useState(false);
+
+	// Event Handler ----------------------------------------------
+	const handleDeleteConfirmBtnOnClick = useCallback(() => {
+		_deleteNotes(_selectedNoteIds);
+		_resetSelectedNoteIds();
+		setIsConfirmOn(false);
+	}, [_selectedNoteIds, _deleteNotes, _resetSelectedNoteIds]);
+
+	const handleMoveToConfirmOnClick = useCallback(() => {
+		setIsConfirmOn(true);
+	}, []);
+
+	// Render ------------------------------------------------------
+	return (
+		<>
+			<div className="align_left">
+				<button className="cancel_btn" onClick={_resetSelectedNoteIds}>
+					<ArrowBackIcon
+						sx={{
+							fontSize: HEADER_ARROWBACK_ICON_FONT_SIZE,
+							color: HEADER_ARROWBACK_ICON_COLOR,
+						}}
+					/>
+				</button>
+				<h2 className="selected_note_count">
+					{_selectedNoteIds.length}
+					{COUNT_OF_SELECTED_NOTE_TEXT}
+				</h2>
+			</div>
+			<button className="delete_btn" onClick={handleMoveToConfirmOnClick}>
+				<DeleteIcon
+					sx={{
+						fontSize: HEADER_DELETE_ICON_FONT_SIZE,
+						color: HEADER_DELETE_ICON_COLOR,
+					}}
+				/>
+			</button>
+			<PortalConfirm
+				question={DO_YOU_WANT_TO_DELETE_SLECTED_NOTES_TEXT}
+				isConfirmOn={isConfirmOn}
+				setIsConfirmOn={setIsConfirmOn}
+				confirmCallback={handleDeleteConfirmBtnOnClick}
+			/>
+		</>
+	);
+};
+
+export default NotesHeader;
