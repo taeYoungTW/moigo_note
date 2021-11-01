3ecd68ce258308087a5760157757847a703f0bf3
TaeYoung Kim <taezero@twinny.ai>
2021-11-01 15:44:13 +0900

    Add: 상수 관리 파일

---
+++ src/constants/constants.js
@@ -0,0 +1,51 @@
+/* 
+TEXTS
+*/
+export const CANCEL_TEXT = '취소'; // PortalConfirm
+export const CONFIRM_TEXT = '확인'; // PortalConfirm
+export const ADD_LIST_TEXT = '항목 추가'; // CreateChecklistBlock
+export const WRITE_NOTE_TEXT = '노트 작성...'; // CreateTextBlock, CreateNote
+export const TITLE_TEXT = '제목'; // CreateNoteForm, UpdateNote
+export const COMPLETE_TEXT = '완료'; // CreateNoteForm, UpdateNote
+export const MOIGO_TEXT = 'MoiGo'; // Header
+export const NOTE_TEXT = '노트'; // Header
+export const SEARCH_TEXT = '검색'; // Header
+export const COUNT_OF_SELECTED_NOTE_TEXT = '개 선택됨'; // Header
+export const DO_YOU_WANT_TO_DELETE_SLECTED_NOTES_TEXT =
+	'선택한 노트를 삭제하시겠습니까?'; // Header, SummaryNote, DetailNote
+
+/* 
+Styles Sizes & Colors & Values
+*/
+export const ADD_BTN_ICON_FONT_SIZE = 22; // AddBtn
+export const ADD_BTN_ICON_COLOR = '#2A394B'; // AddBtn
+
+export const CHECKBOX_ICON_FONT_SIZE = 20; // CreateChecklistBlock
+export const CHECKBOX_ICON_COLOR = 20; // CreateChecklistBlock
+export const TEXT_DECORATION_VALUE = 'solid line-through #767676 1px'; // CreateChecklistBlock
+
+export const CTRL_BLOCK_ICON_FONT_SIZE = 18; // CreateTextBlock, CreateChecklistBLock
+
+export const CTRL_HEADER_BTN_FONT_SIZE = 17; // Header
+export const SEARCH_ICON_FONT_SIZE = 18; // Header
+export const SEARCH_ICON_COLOR = '#767676'; // Header
+export const SEARCH_ICON_MARGIN_RIGHT = '10px'; // Header
+export const HEADER_ARROWBACK_ICON_FONT_SIZE = 19; // Header
+export const HEADER_ARROWBACK_ICON_COLOR = '#767676'; // Header
+export const HEADER_DELETE_ICON_FONT_SIZE = 19; // Header
+export const HEADER_DELETE_ICON_COLOR = '#767676'; // Header
+
+export const SUMMARY_NOTE_DELETE_ICON_FONT_SIZE = 20; // SummaryNote
+export const SUMMARY_NOTE_CHECKCIRCLE_ICON_FONT_SIZE = 35; // SummaryNote
+export const SUMMARY_NOTE_CHECKCIRCLE_ICON_COLOR = '#81C7E4'; // SummaryNote
+
+export const MODAL_NOTE_CLOSE_ICON_FONT_SIZE = 25; // DetailNote, UpdateNote
+export const MODAL_NOTE_CLOSE_ICON_COLOR = '#767676'; // DetailNote, UpdateNote
+
+export const DETAIL_NOTE_DELETE_ICON_FONT_SIZE = 23; // DetailNote
+export const DETAIL_NOTE_DELETE_ICON_COLOR = '#2a394b'; // DetailNote
+
+/* 
+Error Messages
+*/
+export const INVALID_BLOCK_TYPE_TEXT = 'Detected invalid Type of block!';
