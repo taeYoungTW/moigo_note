47fb93d412a4fdef912d1c9caec43fdcbb743f3b
TaeYoung Kim <taezero@twinny.ai>
2021-11-02 10:55:08 +0900

    Mod: content 영역 -> ReadContent 컴포넌트화, deleteNote 버튼 영역 -> DeleteNoteBtn 컴포넌트화 및 연결, "수정" 텍스트 상수화

--- src/components/DetailNote/DetailNote.jsx
+++ src/components/DetailNote/DetailNote.jsx
@@ -1,20 +1,18 @@
 import './DetailNote.scss';
 import CloseIcon from '@mui/icons-material/Close';
-import DeleteIcon from '@mui/icons-material/Delete';
 import { useAppAction, useAppState } from '../../contexts/AppStateContext';
 import { useCallback, useState } from 'react';
-import ReadChecklistBlock from '../ReadBlocks/ReadChecklistBlock';
-import ReadTextBlock from '../ReadBlocks/ReadTextBlock';
 import PortalConfirm from '../Common/PortalConfirm';
-import useError from '../../hooks/useError';
 import {
 	DETAIL_NOTE_DELETE_ICON_COLOR,
 	DETAIL_NOTE_DELETE_ICON_FONT_SIZE,
 	DO_YOU_WANT_TO_DELETE_SLECTED_NOTES_TEXT,
-	INVALID_BLOCK_TYPE_TEXT,
+	EDIT_TEXT,
 	MODAL_NOTE_CLOSE_ICON_COLOR,
 	MODAL_NOTE_CLOSE_ICON_FONT_SIZE,
 } from '../../constants/constants';
+import ReadContent from '../ReadContent/ReadContent';
+import DeleteNoteBtn from '../Common/DeleteBtn';
 
 const DetailNote = ({ setIsEdit }) => {
 	// Global States, Actions ---------------------------------------
@@ -22,12 +20,11 @@ const DetailNote = ({ setIsEdit }) => {
 	const { _resetModalNote, _deleteNote } = useAppAction();
 
 	// Hooks ---------------------------------
-	const _setUseError = useError();
 
 	// Local States ------------------------------------------------
 	const [isConfirmOn, setIsConfrimOn] = useState(false);
 	// Event Handler ----------------------------------------------
-	const handleMoveToConfirmOnClick = useCallback(() => {
+	const handleDeleteBtnOnClick = useCallback(() => {
 		setIsConfrimOn(true);
 	}, []);
 
@@ -57,46 +54,20 @@ const DetailNote = ({ setIsEdit }) => {
 						/>
 					</button>
 				</div>
-				<div className="detailNote_content">
-					{_modalNote?.blocks?.map((block) => {
-						switch (block.type) {
-							case 'text':
-								return (
-									<ReadTextBlock block={block} key={block.id} isDetail={true} />
-								);
-							case 'checklist':
-								return (
-									<ReadChecklistBlock
-										block={block}
-										key={`DetailNote_${block.id}`}
-										noteId={_modalNote.id}
-										isDetail={true}
-									/>
-								);
-							default:
-								_setUseError({
-									message: INVALID_BLOCK_TYPE_TEXT,
-									location: 'DetailNote/detailNote_content_el/switch',
-								});
-								return <></>;
-						}
-					})}
-				</div>
+				<ReadContent note={_modalNote} isDetailNote={true} />
 				<div className="ctrl_bar">
-					<button className="delete_btn" onClick={handleMoveToConfirmOnClick}>
-						<DeleteIcon
-							sx={{
-								fontSize: DETAIL_NOTE_DELETE_ICON_FONT_SIZE,
-								color: DETAIL_NOTE_DELETE_ICON_COLOR,
-							}}
-						/>
-					</button>
+					<DeleteNoteBtn
+						className="delete_btn"
+						handleDeleteBtnOnClick={handleDeleteBtnOnClick}
+						fontSize={DETAIL_NOTE_DELETE_ICON_FONT_SIZE}
+						color={DETAIL_NOTE_DELETE_ICON_COLOR}
+					/>
 					<button
 						type="button"
 						onClick={handleSetEditBtnOnClick}
 						className="edit_btn"
 					>
-						수정
+						{EDIT_TEXT}
 					</button>
 				</div>
 			</div>
