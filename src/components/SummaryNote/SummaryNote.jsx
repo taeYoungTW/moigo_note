99456502728e52d94d68268f284b90fb065eecc6
TaeYoung Kim <taezero@twinny.ai>
2021-11-02 10:59:34 +0900

    Mod: ReadContent, SelectNote, DeleteNoteBtn 컴포넌트 연결 (컴포넌트화)

--- src/components/SummaryNote/SummaryNote.jsx
+++ src/components/SummaryNote/SummaryNote.jsx
@@ -1,42 +1,26 @@
 import './SummaryNote.scss';
-import DeleteIcon from '@mui/icons-material/Delete';
-import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
-import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import { useAppAction, useAppState } from '../../contexts/AppStateContext';
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useState } from 'react';
 import PropTypes from 'prop-types';
-import ReadTextBlock from '../ReadBlocks/ReadTextBlock';
-import ReadChecklistBlock from '../ReadBlocks/ReadChecklistBlock';
 import PortalConfirm from '../Common/PortalConfirm';
-import useError from '../../hooks/useError';
 import {
 	DO_YOU_WANT_TO_DELETE_SLECTED_NOTES_TEXT,
-	SUMMARY_NOTE_CHECKCIRCLE_ICON_COLOR,
-	SUMMARY_NOTE_CHECKCIRCLE_ICON_FONT_SIZE,
 	SUMMARY_NOTE_DELETE_ICON_FONT_SIZE,
 } from '../../constants/constants';
+import ReadContent from '../ReadContent/ReadContent';
+import SelectNote from '../SelectNote/SelectNote';
+import DeleteNoteBtn from '../Common/DeleteBtn';
 
 const SummaryNote = ({ note }) => {
 	// Global States & Actions --------------
-	const {
-		_addSelectedNoteId,
-		_deleteSelectedNoteId,
-		_setModalNote,
-		_deleteNote,
-	} = useAppAction();
+	const { _deleteSelectedNoteId, _setModalNote, _deleteNote } = useAppAction();
 	const { _selectedNoteIds } = useAppState();
 
-	// Hooks ---------------------------------
-	const _setUseError = useError();
-
 	// Local States --------------------------
-	const [isSelected, setIsSelected] = useState(
-		_selectedNoteIds.includes(note.id)
-	); // false로 해야하나 고민
 	const [isConfirmOn, setIsConfirmOn] = useState(false);
 
 	// Event Handler ----------------------------------------------
-	const handleMoveToConfirmOnClick = useCallback((e) => {
+	const handleDeleteBtnOnClick = useCallback((e) => {
 		e.stopPropagation();
 		setIsConfirmOn(true);
 	}, []);
@@ -49,27 +33,7 @@ const SummaryNote = ({ note }) => {
 		if (isSelected) {
 			_deleteSelectedNoteId(id);
 		}
-		// CreatePortal: modal 사용 -> 전체 rerendering X , 효율적
 	};
-
-	// Select or Unselect A SummaryNote
-	/* 
-	- 통합 필요 여지 O
-	*/
-	const handleSelectBtnOnClick = useCallback(() => {
-		setIsSelected(true);
-		_addSelectedNoteId(note.id);
-	}, [_addSelectedNoteId, note]);
-
-	const handleUnselectBtnOnClick = useCallback(() => {
-		setIsSelected(false);
-		_deleteSelectedNoteId(note.id);
-	}, [_deleteSelectedNoteId, note]);
-
-	//---- useEffect ---- For Cancel Selection (Update Note Outline)
-	useEffect(() => {
-		setIsSelected(_selectedNoteIds.includes(note.id));
-	}, [_selectedNoteIds, note]);
 
 	// Render -------------------------------------------
 	return (
@@ -81,72 +45,15 @@ const SummaryNote = ({ note }) => {
 		>
 			<div className="summary_area">
 				{note.title && <h1 className="summary_title">{note.title}</h1>}
-				{note?.blocks?.map((block) => {
-					switch (block.type) {
-						case 'text':
-							return (
-								<ReadTextBlock block={block} key={block.id} isDetail={false} />
-							);
-						case 'checklist':
-							return (
-								<ReadChecklistBlock
-									block={block}
-									key={block.id}
-									noteId={note.id}
-									isDetail={false}
-								/>
-							);
-						default:
-							_setUseError({
-								message: 'Detected invalid Type of blocks!',
-								location: 'SummaryNote/summary_area_el/switch',
-							});
-							return <></>;
-					}
-				})}
+				<ReadContent note={note} isDetailNote={false} />
 			</div>
 			<div className="ctrl_area">
-				<button
+				<DeleteNoteBtn
 					className="del_summary_note_btn"
-					onClickCapture={handleMoveToConfirmOnClick}
-				>
-					<DeleteIcon sx={{ fontSize: SUMMARY_NOTE_DELETE_ICON_FONT_SIZE }} />
-				</button>
-
-				{isSelected ? (
-					<>
-						{/* 식별자명 수정 필요 */}
-						<button
-							className="select_summary_note_off_btn"
-							onClickCapture={handleUnselectBtnOnClick}
-						>
-							<CheckCircleIcon
-								sx={{
-									fontSize: SUMMARY_NOTE_CHECKCIRCLE_ICON_FONT_SIZE,
-									color: SUMMARY_NOTE_CHECKCIRCLE_ICON_COLOR,
-								}}
-							/>
-						</button>
-						<div
-							className="select_outline"
-							onClick={(e) => {
-								e.stopPropagation();
-							}}
-						></div>
-					</>
-				) : (
-					<button
-						className="select_summary_note_on_btn"
-						onClickCapture={handleSelectBtnOnClick}
-					>
-						<CheckCircleOutlineIcon
-							sx={{
-								fontSize: SUMMARY_NOTE_CHECKCIRCLE_ICON_FONT_SIZE,
-								color: SUMMARY_NOTE_CHECKCIRCLE_ICON_COLOR,
-							}}
-						/>
-					</button>
-				)}
+					handleDeleteBtnOnClick={handleDeleteBtnOnClick}
+					fontSize={SUMMARY_NOTE_DELETE_ICON_FONT_SIZE}
+				/>
+				<SelectNote noteId={note.id} />
 			</div>
 			<PortalConfirm
 				question={DO_YOU_WANT_TO_DELETE_SLECTED_NOTES_TEXT}
