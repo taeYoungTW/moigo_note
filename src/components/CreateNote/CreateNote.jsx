99c865964dd130d0d7fb8227483be66f344ba5dc
TaeYoung Kim <taezero@twinny.ai>
2021-11-02 16:01:25 +0900

    Mod: handleAddBlockBtnOnClick 이라는 이벤트 핸들러 재사용을 위해 hooks로 만들어 사용 -> 연결

--- src/components/CreateNote/CreateNote.jsx
+++ src/components/CreateNote/CreateNote.jsx
@@ -3,31 +3,18 @@ import InsertPhotoIcon from '@mui/icons-material/InsertPhoto';
 import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
 import AddBtn from '../Common/AddBtn';
 import { useAppAction } from '../../contexts/AppStateContext';
-import { v4 as uuid } from 'uuid';
-import { useCallback } from 'react';
 import { WRITE_NOTE_TEXT } from '../../constants/constants';
+import useAddBlock from '../../hooks/useAddBlock';
 
 const CreateNote = () => {
 	// Global States & Actions --------------------------
 	const { _changeIsOnCreateNoteForm, _addBlock } = useAppAction();
 
 	// Event Handler --------------------------
-	const handleAddBlockBtnOnClick = useCallback(
-		(type) => {
-			_changeIsOnCreateNoteForm(true);
-			switch (type) {
-				case 'text':
-					_addBlock({ id: uuid(), type, text: '' });
-					break;
-				case 'checklist':
-					_addBlock({ id: uuid(), type, isDone: false, content: '' });
-					break;
-				default:
-					break;
-			}
-		},
-		[_changeIsOnCreateNoteForm, _addBlock]
-	);
+	// hook을 사용한 Event Handler
+	const handleAddBlockBtnOnClick = useAddBlock(_addBlock, () => {
+		_changeIsOnCreateNoteForm(true);
+	});
 
 	// Render -----------------------
 	return (
