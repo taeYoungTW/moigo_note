94ab425d1afe692a92bc26b0e41b1a50adc01221
TaeYoung Kim <taezero@twinny.ai>
2021-11-02 16:15:49 +0900

    Add: 노트 자체에서 자신의 노트의 내용 중 찾고 있는 값인지 확인 가능한 useSearch hook 생성

---
+++ src/hooks/useSearch.jsx
@@ -0,0 +1,42 @@
+import { useCallback, useEffect, useState } from 'react';
+
+const useSearch = (searchValue, targetNote) => {
+	const [isDisplay, setIsDisplay] = useState(true);
+	const { title, blocks } = targetNote;
+	const checkTitleSummaryNote = useCallback(
+		(value) => {
+			if (value) {
+				return title.includes(value);
+			} else {
+				return true;
+			}
+		},
+		[title]
+	);
+
+	const checkBlocksSummaryNote = useCallback(
+		(value) => {
+			if (value) {
+				for (let i = 0; i < blocks.length; i++) {
+					const curBlock = blocks[i];
+					const curBlockText = curBlock?.text || curBlock?.content;
+					if (curBlockText.includes(value)) {
+						return true;
+					}
+				}
+			} else {
+				return true;
+			}
+		},
+		[blocks]
+	);
+
+	useEffect(() => {
+		setIsDisplay(
+			checkTitleSummaryNote(searchValue) || checkBlocksSummaryNote(searchValue)
+		);
+	}, [searchValue, checkTitleSummaryNote, checkBlocksSummaryNote]);
+	return isDisplay;
+};
+
+export default useSearch;
