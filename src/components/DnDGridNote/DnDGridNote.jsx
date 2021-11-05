fe236f9c524d1bb4a41c81c9f48627f5519c0f82
TaeYoung Kim <taezero@twinny.ai>
2021-11-05 15:32:42 +0900

    Add : 드래그 앤 드롭을 인식하여 SummaryNote의 순서 변경을 실행 시키는 HOC 컴포넌트

---
+++ src/components/DnDGridNote/DnDGridNote.jsx
@@ -0,0 +1,41 @@
+import React, { useRef } from 'react';
+import { useDrag, useDrop } from 'react-dnd';
+
+const NOTE_TYPE = 'NOTE_DND';
+
+const DnDGridNote = ({ id, index, note, moveNotes, Component }) => {
+	const noteRef = useRef(null);
+
+	const [, drop] = useDrop({
+		accept: NOTE_TYPE,
+		hover(dragItem, monitor) {
+			const { dragIndex } = dragItem;
+
+			if (dragIndex === index) {
+				return;
+			}
+
+			moveNotes(dragIndex, index);
+			dragItem.dragIndex = index;
+		},
+	});
+
+	const [{ isDragging }, drag] = useDrag({
+		type: NOTE_TYPE,
+		item: () => ({ dragId: id, dragIndex: index }),
+		collect: (monitor) => {
+			return {
+				isDragging: monitor.isDragging(),
+			};
+		},
+	});
+
+	drag(drop(noteRef));
+	return (
+		<div ref={noteRef}>
+			<Component isDragging={isDragging} note={note} />
+		</div>
+	);
+};
+
+export default DnDGridNote;
