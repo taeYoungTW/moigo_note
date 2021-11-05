ae336e7e32a60026d37417da1ec305bd0c295f1f
TaeYoung Kim <taezero@twinny.ai>
2021-11-05 15:33:46 +0900

    Mod: react-dnd 연결 및 DnDGridNote 컴포넌트 연결

--- src/components/ReadAllNotes/ReadAllNotes.jsx
+++ src/components/ReadAllNotes/ReadAllNotes.jsx
@@ -1,19 +1,41 @@
-import { useAppState } from '../../contexts/AppStateContext';
-import SummaryNote from '../SummaryNote/SummaryNote';
+import { useAppAction, useAppState } from '../../contexts/AppStateContext';
 import './ReadAllNotes.scss';
 import ModalNote from '../ModalNote/ModalNote';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import DnDGridNote from '../DnDGridNote/DnDGridNote';
+import { useCallback } from 'react';
+import SummaryNote from '../SummaryNote/SummaryNote';
 
 const ReadAllNotes = () => {
 	// Global States, Actions ---------------------------------------
 	const { _allNotes, _modalNote } = useAppState();
-
+	const { _setNotes } = useAppAction();
+	const moveNotes = useCallback(
+		(dragIndex, hoverIndex) => {
+			const newNotes = [..._allNotes];
+			const [dragedNote] = newNotes.splice(dragIndex, 1);
+			newNotes.splice(hoverIndex, 0, dragedNote);
+			_setNotes(newNotes);
+		},
+		[_allNotes, _setNotes]
+	);
 	// Render -----------------------------------------------
 	return (
 		<section className="read_all_notes">
-			{_allNotes.map((note) => (
-				<SummaryNote note={note} key={note.id} />
-			))}
-			{_modalNote?.id && <ModalNote />}
+			<DndProvider backend={HTML5Backend}>
+				{_allNotes.map((note, i) => (
+					<DnDGridNote
+						key={note.id}
+						id={note.id}
+						index={i}
+						moveNotes={moveNotes}
+						Component={SummaryNote}
+						note={note}
+					/>
+				))}
+				{_modalNote?.id && <ModalNote />}
+			</DndProvider>
 		</section>
 	);
 };
