d58fe6a60c4a798909e463b3e1a61d295aee017e
TaeYoung Kim <taezero@twinny.ai>
2021-11-05 17:16:02 +0900

    Mod: 기존의 CreateChecklistBlock, CreateTextBlock 모두 DnDListBlockHOC 컴포넌트로 입힘

--- src/components/CreateContent/CreateContent.jsx
+++ src/components/CreateContent/CreateContent.jsx
@@ -3,31 +3,33 @@ import { INVALID_BLOCK_TYPE_TEXT } from '../../constants/constants';
 import useError from '../../hooks/useError';
 import CreateChecklistBlock from '../CreateBlocks/CreateChecklistBlock';
 import CreateTextBlock from '../CreateBlocks/CreateTextBlock';
+import DnDListBlockHOC from '../DnDListBlockHOC/DnDListBlockHOC';
 
 const CreateContent = ({ blocks, isUpdateNote }) => {
 	const _setUseError = useError();
 
 	return (
 		<div className="content">
-			{blocks?.map((block) => {
+			{blocks?.map((block, i) => {
 				switch (block.type) {
 					case 'text':
 						return (
-							<CreateTextBlock
-								block={block}
-								key={`${isUpdateNote ? 'UpdateNote_' : 'CreateNoteForm_'}${
-									block.id
-								}`}
+							<DnDListBlockHOC
+								Component={CreateTextBlock}
+								ComponentProp={{ block: block }}
+								key={block.id}
+								id={block.id}
+								index={i}
 							/>
 						);
 					case 'checklist':
 						return (
-							<CreateChecklistBlock
-								block={block}
-								key={`${isUpdateNote ? 'UpdateNote_' : 'CreateNoteForm_'}${
-									block.id
-								}`}
-								isUpdate={true}
+							<DnDListBlockHOC
+								Component={CreateChecklistBlock}
+								ComponentProp={{ block: block, isUpdate: isUpdateNote }}
+								key={block.id}
+								id={block.id}
+								index={i}
 							/>
 						);
 					default:
