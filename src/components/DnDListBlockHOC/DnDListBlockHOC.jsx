178cc52f1e42b6830dfd600bd3230a33880b3911
TaeYoung Kim <taezero@twinny.ai>
2021-11-09 10:41:08 +0900

    Mod: block location indicator 구현 및 MoveBtn 컴포넌트 컴포넌트화 해제하여 연결 (컴포넌트시 dnd 로직 짜기가 어려워 집니다.)

--- src/components/DnDListBlockHOC/DnDListBlockHOC.jsx
+++ src/components/DnDListBlockHOC/DnDListBlockHOC.jsx
@@ -1,9 +1,12 @@
 import React, { useCallback, useRef } from 'react';
-import { useDrop } from 'react-dnd';
+import { useDrag, useDrop } from 'react-dnd';
 import { useAppAction, useAppState } from '../../contexts/AppStateContext';
 
-import { BLOCK_TYPE } from '../../constants/constants';
-import MoveBlockBtn from '../Common/MoveBlockBtn';
+import {
+	BLOCK_TYPE,
+	CTRL_BLOCK_ICON_FONT_SIZE,
+} from '../../constants/constants';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const DnDListBlockHOC = ({ id, index, Component, ComponentProp }) => {
 	// Global States & Actions
@@ -31,13 +34,16 @@ const DnDListBlockHOC = ({ id, index, Component, ComponentProp }) => {
 				return;
 			}
 
-			const { y: dropY, height: dropHeight } =
-				dropRef.current.getBoundingClientRect();
+			const {
+				top: dropTop,
+				bottom: dropBottom,
+				height: dropHeight,
+			} = dropRef.current.getBoundingClientRect();
 			const { y: dragCursorY } = monitor.getClientOffset();
 
 			if (dragIndex > index) {
-				if (dropHeight > 45) {
-					if (dragCursorY < dropY + 45) {
+				if (dropHeight > 10) {
+					if (dragCursorY < dropTop + 10) {
 						moveBlock(dragIndex, index);
 						dragItem.dragIndex = index;
 						return;
@@ -45,16 +51,51 @@ const DnDListBlockHOC = ({ id, index, Component, ComponentProp }) => {
 					return;
 				}
 			}
-			moveBlock(dragIndex, index);
-			dragItem.dragIndex = index;
+
+			if (dragIndex < index) {
+				if (dropHeight > 10) {
+					if (dragCursorY > dropBottom - 10) {
+						moveBlock(dragIndex, index);
+						dragItem.dragIndex = index;
+						return;
+					}
+					return;
+				}
+			}
+
+			// moveBlock(dragIndex, index);
+			// dragItem.dragIndex = index;
+		},
+	});
+
+	const [{ isDragging }, drag, preview] = useDrag({
+		type: BLOCK_TYPE,
+		item: () => ({ dragId: id, dragIndex: index }),
+		collect: (monitor) => {
+			return {
+				isDragging: monitor.isDragging(),
+			};
 		},
 	});
 
 	return (
 		<div ref={drop(dropRef)}>
-			<Component {...ComponentProp}>
-				<MoveBlockBtn index={index} id={id} />
-			</Component>
+			<div
+				className="block_location_indicator"
+				style={{
+					height: '10px',
+					backgroundColor: '#f8613b',
+					opacity: 0.5,
+					display: isDragging ? '' : 'none',
+				}}
+			></div>
+			<div style={{ display: isDragging ? 'none' : '' }} ref={preview}>
+				<Component {...ComponentProp}>
+					<button type="button" ref={drag}>
+						<MenuIcon sx={{ fontSize: CTRL_BLOCK_ICON_FONT_SIZE }} />
+					</button>
+				</Component>
+			</div>
 		</div>
 	);
 };
