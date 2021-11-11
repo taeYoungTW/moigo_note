c1246b1fde54ab1e124d089776c147f2345d4a9b
TaeYoung Kim <taezero@twinny.ai>
2021-11-11 17:24:27 +0900

    Add: material ui icon style 상수 값 관리

---
+++ src/constants/iconStyles.js
@@ -0,0 +1,47 @@
+export const ADD_BTN_ICON_STYLE = {
+	fontSize: 22,
+	cursor: 'pointer',
+	color: '#2A394B',
+};
+
+export const CHECKBOX_ICON_STYLE = {
+	fontSize: 20,
+	color: '#2A394B',
+};
+
+export const CHECKLIST_CONTENT_DECORATION_VALUE =
+	'solid line-through #767676 1px';
+
+export const CTRL_BLOCK_ICON_STYLE = { fontSize: 18 };
+
+export const HEADER_DELETE_ICON_STYLE = {
+	fontSize: 19,
+	color: '#767676',
+};
+
+export const SUMMARY_NOTE_DELETE_ICON_STYLE = { fontSize: 20 };
+
+export const DETAIL_NOTE_DELETE_ICON_STYLE = { fontSize: 23, color: '#2a394b' };
+
+export const CTRL_WINDOW_BTN_ICON_STYLE = { fontSize: 17, cursor: 'pointer' };
+
+export const HEADER_SEARCH_ICON_STYLE = {
+	fontSize: 18,
+	color: '#767676',
+	marginRight: '10px',
+};
+
+export const HEADER_ARROWBACK_ICON_STYLE = {
+	fontSize: 19,
+	color: '#767676',
+};
+
+export const SUMMARY_NOTE_CHECKCIRCLE_ICON_STYLE = {
+	fontSize: 35,
+	color: '#81C7E4',
+};
+
+export const MODAL_NOTE_CLOSE_ICON_STYLE = {
+	fontSize: 25,
+	color: '#767676',
+};
