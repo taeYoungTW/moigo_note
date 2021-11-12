044749ac4fcc2479b780599a8d108a2daa004e9c
TaeYoung Kim <taezero@twinny.ai>
2021-11-12 13:43:30 +0900

    Mod: useAutoHeightTextArea -> useAutoHeightTextarea 로 식별자 변경

--- src/hooks/useAutoHeightTextArea.jsx
+++ src/hooks/useAutoHeightTextArea.jsx
@@ -1,10 +1,10 @@
 import { useEffect } from 'react';
 
-const useAutoHeightTextArea = (targetRef, text) => {
+const useAutoHeightTextarea = (targetRef, text) => {
 	useEffect(() => {
 		targetRef.current.style.height = '';
 		targetRef.current.style.height = targetRef.current.scrollHeight + 'px';
 	}, [text, targetRef]);
 };
 
-export default useAutoHeightTextArea;
+export default useAutoHeightTextarea;
