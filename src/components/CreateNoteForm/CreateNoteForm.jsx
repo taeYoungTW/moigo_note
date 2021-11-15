19f2648857511ff4da6b64e7a6957feaa10d50e2
TaeYoung Kim <taezero@twinny.ai>
2021-11-15 17:25:36 +0900

    Mod: _isOnCreateNoteForm -> _isCreateNoteFormOn으로 식별자 수정 및 ctrl_bar 부분 CreateCtrlBar 컴포넌트로 분리

--- src/components/CreateNoteForm/CreateNoteForm.jsx
+++ src/components/CreateNoteForm/CreateNoteForm.jsx
@@ -1,22 +1,16 @@
-import AddBtn from '../Common/AddBtn';
 import './CreateNoteForm.scss';
-import InsertPhotoIcon from '@mui/icons-material/InsertPhoto';
-import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
-import TextFieldsIcon from '@mui/icons-material/TextFields';
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { v4 as uuid } from 'uuid';
 import { useAppAction, useAppState } from '../../contexts/AppStateContext';
 import { COMPLETE_TEXT, TITLE_TEXT } from '../../constants/constants';
 import { emptyTextBlockFilter } from '../../utils/emptyTextBlockFilter';
 import CreateContent from '../CreateContent/CreateContent';
-import useAddDefaultBlock from '../../hooks/useAddDefaultBlock';
-import ImgInput from '../Common/ImgInput';
+import CreateCtrlBar from '../CreateCtrlBar/CreateCtrlBar';
 
 const CreateNoteForm = () => {
 	// Global States & Actions --------------------------
 	const { _blocks } = useAppState();
-	const { _addNote, _changeIsOnCreateNoteForm, _addTypeBlock, _resetBlocks } =
-		useAppAction();
+	const { _addNote, _setIsCreateNoteFormOn, _resetBlocks } = useAppAction();
 
 	// Local State -------------------------------------
 	const [note, setNote] = useState({
@@ -28,7 +22,7 @@ const CreateNoteForm = () => {
 	const handleCreateNoteBtnOnClick = () => {
 		const filteredBlocks = emptyTextBlockFilter(_blocks);
 		_addNote({ ...note, id: uuid(), blocks: [...filteredBlocks] });
-		_changeIsOnCreateNoteForm(false);
+		_setIsCreateNoteFormOn(false);
 	};
 
 	const handleTitleInputOnChange = (e) => {
@@ -36,15 +30,7 @@ const CreateNoteForm = () => {
 		setNote({ title: value });
 	};
 
-	const handleAddBlockBtnOnClick = useCallback(
-		(type, dataUrl) => {
-			_addTypeBlock(type, dataUrl);
-		},
-		[_addTypeBlock]
-	);
-
 	// useEffects ------------------------------------
-	useAddDefaultBlock(_addTypeBlock, _blocks.length); // hooks로 재사용 관리
 
 	useEffect(() => {
 		return () => {
@@ -65,36 +51,10 @@ const CreateNoteForm = () => {
 				/>
 			</div>
 			<CreateContent blocks={_blocks} isUpdateNote={false} />
-			<div className="ctrl_bar">
-				<div className="add_btns">
-					<ImgInput
-						callback={(dataUrl) => {
-							handleAddBlockBtnOnClick('image', dataUrl);
-						}}
-					>
-						<AddBtn Icon={InsertPhotoIcon} isImgBtn />
-					</ImgInput>
-					<AddBtn
-						Icon={FormatListBulletedIcon}
-						eventHandler={() => {
-							handleAddBlockBtnOnClick('checklist');
-						}}
-					/>
-					<AddBtn
-						Icon={TextFieldsIcon}
-						eventHandler={() => {
-							handleAddBlockBtnOnClick('text');
-						}}
-					/>
-				</div>
-				<button
-					type="button"
-					onClick={handleCreateNoteBtnOnClick}
-					className="create_submit_btn"
-				>
-					{COMPLETE_TEXT}
-				</button>
-			</div>
+			<CreateCtrlBar
+				handleSubmitBtnOnClick={handleCreateNoteBtnOnClick}
+				submitBtnName={COMPLETE_TEXT}
+			/>
 		</section>
 	);
 };
