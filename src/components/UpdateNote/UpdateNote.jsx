5a0fab013565b3a12d44ab83bc4fe17d72bac596
TaeYoung Kim <taezero@twinny.ai>
2021-11-15 17:30:44 +0900

    Mod: ctrl_bar -> CreateCtrlBar 컴포넌트화 시켜 분리, useAddDefaultBlock hook은 CreateContent로 이동

--- src/components/UpdateNote/UpdateNote.jsx
+++ src/components/UpdateNote/UpdateNote.jsx
@@ -1,28 +1,18 @@
 import CloseIcon from '@mui/icons-material/Close';
 import { useAppAction, useAppState } from '../../contexts/AppStateContext';
-import AddBtn from '../Common/AddBtn';
-import InsertPhotoIcon from '@mui/icons-material/InsertPhoto';
-import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
-import TextFieldsIcon from '@mui/icons-material/TextFields';
 import './UpdateNote.scss';
 import { useCallback, useEffect, useState } from 'react';
 import { COMPLETE_TEXT, TITLE_TEXT } from '../../constants/constants';
 import { emptyTextBlockFilter } from '../../utils/emptyTextBlockFilter';
 import CreateContent from '../CreateContent/CreateContent';
-import useAddDefaultBlock from '../../hooks/useAddDefaultBlock';
-import ImgInput from '../Common/ImgInput';
 import { MODAL_NOTE_CLOSE_ICON_STYLE } from '../../constants/iconStyles';
+import CreateCtrlBar from '../CreateCtrlBar/CreateCtrlBar';
 
 const UpdateNote = () => {
 	// Global States, Actions ---------------------------------------
 	const { _blocks, _modalNote } = useAppState();
-	const {
-		_resetModalNote,
-		_updateNote,
-		_initBlocks,
-		_resetBlocks,
-		_addTypeBlock,
-	} = useAppAction();
+	const { _resetModalNote, _updateNote, _initBlocks, _resetBlocks } =
+		useAppAction();
 
 	// Local States ------------------------------------------------
 	const [note, setNote] = useState(_modalNote);
@@ -38,15 +28,7 @@ const UpdateNote = () => {
 		setNote((note) => ({ ...note, title: value }));
 	}, []);
 
-	const handleAddBlockBtnOnClick = useCallback(
-		(type, dataUrl) => {
-			_addTypeBlock(type, dataUrl);
-		},
-		[_addTypeBlock]
-	);
-
 	// useEffects ------------------------------------------------------
-	useAddDefaultBlock(_addTypeBlock, _blocks.length); // hooks로 재사용 관리
 
 	useEffect(() => {
 		_initBlocks([..._modalNote.blocks]);
@@ -72,37 +54,11 @@ const UpdateNote = () => {
 					</button>
 				</div>
 				<CreateContent blocks={_blocks} isUpdateNote={true} />
-				<div className="ctrl_bar">
-					<div className="add_btns">
-						<ImgInput
-							callback={(dataUrl) => {
-								handleAddBlockBtnOnClick('image', dataUrl);
-							}}
-							isUpdate={true}
-						>
-							<AddBtn Icon={InsertPhotoIcon} isImgBtn />
-						</ImgInput>
-						<AddBtn
-							Icon={FormatListBulletedIcon}
-							eventHandler={() => {
-								handleAddBlockBtnOnClick('checklist');
-							}}
-						/>
-						<AddBtn
-							Icon={TextFieldsIcon}
-							eventHandler={() => {
-								handleAddBlockBtnOnClick('text');
-							}}
-						/>
-					</div>
-					<button
-						type="button"
-						onClick={handleUpdateNoteBtnOnClick}
-						className="update_submit_btn"
-					>
-						{COMPLETE_TEXT}
-					</button>
-				</div>
+				<CreateCtrlBar
+					handleSubmitBtnOnClick={handleUpdateNoteBtnOnClick}
+					submitBtnName={COMPLETE_TEXT}
+					isUpdate
+				/>
 			</div>
 		</>
 	);
