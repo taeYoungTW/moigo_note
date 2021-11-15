be0a3ad541c4110073084cb912714a4a5087aaa4
TaeYoung Kim <taezero@twinny.ai>
2021-11-15 17:27:35 +0900

    Mod: _isOnCreateNoteForm -> _isCreateNoteFormOn으로 식별자 수정 및 handleMainOnClick 내부 로직 최적화(_isCreateNoteFormOn이 false이면 실행 하지 않음)

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -5,8 +5,8 @@ import ReadAllNotes from '../ReadAllNotes/ReadAllNotes';
 import './Main.scss';
 const Main = () => {
 	// Global States, Actions ---------------------------------------
-	const { _changeIsOnCreateNoteForm } = useAppAction();
-	const { _blocks } = useAppState();
+	const { _setIsCreateNoteFormOn } = useAppAction();
+	const { _isCreateNoteFormOn, _blocks } = useAppState();
 
 	// Event Handler ----------------------------------------------
 	/* - handleMainOnClick
@@ -16,14 +16,18 @@ const Main = () => {
 	 * 조건: 블록의 개수가 1개 이면서, 해당 블럭의 text 또는 content의 내용이 없는 경우
 	 */
 	const handleMainOnClick = useCallback(() => {
+		if (!_isCreateNoteFormOn) {
+			return;
+		}
+
 		const firstBlock = _blocks[0];
 		if (
 			_blocks.length === 1 &&
 			(firstBlock.text === '' || firstBlock.content === '')
 		) {
-			_changeIsOnCreateNoteForm(false);
+			_setIsCreateNoteFormOn(false);
 		}
-	}, [_blocks, _changeIsOnCreateNoteForm]);
+	}, [_blocks, _setIsCreateNoteFormOn, _isCreateNoteFormOn]);
 
 	// Render -----------------------------------------------------
 	return (
