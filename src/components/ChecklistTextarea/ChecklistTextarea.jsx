79939a0b861921a55d23af487e121e40b1030bd6
TaeYoung Kim <taezero@twinny.ai>
2021-11-16 17:45:26 +0900

    Mod: 독립된 폴더로 위치 이동

---
+++ src/components/ChecklistTextarea/ChecklistTextarea.jsx
@@ -0,0 +1,37 @@
+import { useRef } from 'react';
+import { ADD_LIST_TEXT } from '../../constants/constants';
+import { CHECKLIST_CONTENT_DECORATION_VALUE } from '../../constants/iconStyles';
+import useAutoHeightTextarea from '../../hooks/useAutoHeightTextarea';
+import './ChecklistTextarea.scss';
+const ChecklistTextarea = ({
+	content,
+	isDone,
+	handleChecklistContentOnChange,
+	handleOnKeyDown,
+}) => {
+	const contentRef = useRef(null);
+
+	// hook : textarea auto height -----------------------------
+	useAutoHeightTextarea(contentRef, content);
+
+	// Render ------------------------------------------
+	return (
+		<textarea
+			className="checklist_block_textarea"
+			type="text"
+			value={content}
+			onChange={handleChecklistContentOnChange}
+			onKeyDown={handleOnKeyDown}
+			placeholder={ADD_LIST_TEXT}
+			rows={1}
+			ref={contentRef}
+			spellCheck={false}
+			autoFocus={true}
+			style={{
+				textDecoration: isDone && CHECKLIST_CONTENT_DECORATION_VALUE,
+			}}
+		/>
+	);
+};
+
+export default ChecklistTextarea;
