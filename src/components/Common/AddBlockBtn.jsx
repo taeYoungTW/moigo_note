a8763c69e1abe5f2033acb2a91dc34e90fe25a2f
TaeYoung Kim <taezero@twinny.ai>
2021-11-16 12:25:08 +0900

    Mod: AddBtn에서 AddBlockBtn으로 식별자 수정 및 button 태그에서 div태그로 수정

---
+++ src/components/Common/AddBlockBtn.jsx
@@ -0,0 +1,24 @@
+import './AddBlockBtn.scss';
+import PropTypes from 'prop-types';
+import { ADD_BTN_ICON_STYLE } from '../../constants/iconStyles';
+
+const AddBlockBtn = ({ Icon, eventHandler }) => {
+	return (
+		<div className="add_btn_icon_ctnr">
+			<Icon sx={ADD_BTN_ICON_STYLE} onClick={eventHandler} />
+		</div>
+	);
+};
+
+AddBlockBtn.propTypes = {
+	Icon: PropTypes.elementType,
+	/*
+	 * - elementType
+	 * 식별자 명으로 prop 전달 O
+	 * jsx 스타일 컴포넌트 표기법으로 prop 전달 X
+	 * - element : elementType 반대
+	 */
+	eventHandler: PropTypes.func,
+};
+
+export default AddBlockBtn;
