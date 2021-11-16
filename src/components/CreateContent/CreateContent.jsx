bb1d2e1bf7ed4c1f933d247f3571dcef29796137
TaeYoung Kim <taezero@twinny.ai>
2021-11-16 12:27:12 +0900

    Mod : 기존 block type 매칭 로직 CreateBlock으로 이동, blocks를 열거하며 DnD와 CreateBlock을 연결하는 역할만 하는 로직으로 수정

--- src/components/CreateContent/CreateContent.jsx
+++ src/components/CreateContent/CreateContent.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
-import { INVALID_BLOCK_TYPE_TEXT } from '../../constants/constants';
-import useError from '../../hooks/useError';
-import CreateChecklistBlock from '../CreateBlocks/CreateChecklistBlock';
-import CreateImgBlock from '../CreateBlocks/CreateImgBlock';
-import CreateTextBlock from '../CreateBlocks/CreateTextBlock';
 import DnDListBlockHOC from '../DnDListBlockHOC/DnDListBlockHOC';
 import { useAppAction } from '../../contexts/AppStateContext';
 import useAddDefaultBlock from '../../hooks/useAddDefaultBlock';
+import CreateBlock from '../CreateBlocks/CreateBlock';
 
 const CreateContent = ({ blocks, isUpdateNote }) => {
 	// Global States & Actions --------------------------
@@ -15,52 +11,17 @@ const CreateContent = ({ blocks, isUpdateNote }) => {
 	// Hooks -----------------------------------------
 	useAddDefaultBlock(_addTypeBlock, blocks.length);
 
-	const _setUseError = useError();
-
 	return (
 		<div className="content">
-			{blocks?.map((block, i) => {
-				switch (block.type) {
-					case 'text':
-						return (
-							<DnDListBlockHOC
-								Component={CreateTextBlock}
-								ComponentProp={{ block }}
-								key={block.id}
-								id={block.id}
-								index={i}
-							/>
-						);
-					case 'checklist':
-						return (
-							<DnDListBlockHOC
-								Component={CreateChecklistBlock}
-								ComponentProp={{ block, isUpdate: isUpdateNote }}
-								key={block.id}
-								id={block.id}
-								index={i}
-							/>
-						);
-					case 'image':
-						return (
-							<DnDListBlockHOC
-								Component={CreateImgBlock}
-								ComponentProp={{ block, isUpdate: isUpdateNote }}
-								key={block.id}
-								id={block.id}
-								index={i}
-							/>
-						);
-					default:
-						_setUseError({
-							message: INVALID_BLOCK_TYPE_TEXT,
-							location: `${
-								isUpdateNote ? 'UpdateNote' : 'CreateNoteForm'
-							}/CreateContent/switch`,
-						});
-						return <></>;
-				}
-			})}
+			{blocks?.map((block, i) => (
+				<DnDListBlockHOC
+					Component={CreateBlock}
+					ComponentProp={{ block, isUpdateNote }}
+					key={block.id}
+					id={block.id}
+					index={i}
+				/>
+			))}
 		</div>
 	);
 };
