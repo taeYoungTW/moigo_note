1d5a607fb937fad391e4e62b6a4ce6aaad6c4427
TaeYoung Kim <taezero@twinny.ai>
2021-11-16 12:22:19 +0900

    Mod: 각 type 별 block Component에서 move, delete 버튼 컴포넌트화시켜 분리, 블록 외형에 관한 요소를 CreateBlock으로 이동

--- src/components/CreateBlocks/CreateImgBlock.jsx
+++ src/components/CreateBlocks/CreateImgBlock.jsx
@@ -1,27 +1,5 @@
-import React, { useCallback } from 'react';
-import { useAppAction } from '../../contexts/AppStateContext';
-import DeleteBtn from '../Common/DeleteBtn';
-import { CTRL_BLOCK_ICON_STYLE } from '../../constants/iconStyles';
-
 const CreateImgBlock = ({ block, children }) => {
-	const { _deleteBlock } = useAppAction();
-
-	const handleDeleteBtnOnClick = useCallback(() => {
-		_deleteBlock(block.id);
-	}, [_deleteBlock, block]);
-
-	return (
-		<div className="create_block">
-			<img src={block.dataUrl} alt="img_block" className="img" />
-			<div className="btns">
-				<DeleteBtn
-					style={CTRL_BLOCK_ICON_STYLE}
-					handleDeleteBtnOnClick={handleDeleteBtnOnClick}
-				/>
-				{children}
-			</div>
-		</div>
-	);
+	return <img src={block.dataUrl} alt="img_block" className="img" />;
 };
 
 export default CreateImgBlock;
