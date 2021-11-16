d2ada4d25e4e77503c4aa530935864be5e1a81ab
TaeYoung Kim <taezero@twinny.ai>
2021-11-16 17:45:18 +0900

    Mod: 독립된 폴더로 위치 이동

---
+++ src/components/CheckBoxInput/CheckBoxInput.jsx
@@ -0,0 +1,32 @@
+import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
+import CheckBoxIcon from '@mui/icons-material/CheckBox';
+import './CheckBoxInput.scss';
+import { CHECKBOX_ICON_STYLE } from '../../constants/iconStyles';
+
+const CheckBoxInput = ({
+	blockId,
+	isDone,
+	location,
+	handleCheckBoxOnChange,
+}) => {
+	return (
+		<>
+			<label htmlFor={`${location}_${blockId}`} className="checkbox_label">
+				{isDone ? (
+					<CheckBoxIcon sx={CHECKBOX_ICON_STYLE} />
+				) : (
+					<CheckBoxOutlineBlankIcon sx={CHECKBOX_ICON_STYLE} />
+				)}
+			</label>
+			<input
+				type="checkbox"
+				id={`${location}_${blockId}`}
+				className="checkbox_input"
+				checked={isDone}
+				onChange={handleCheckBoxOnChange}
+			/>
+		</>
+	);
+};
+
+export default CheckBoxInput;
