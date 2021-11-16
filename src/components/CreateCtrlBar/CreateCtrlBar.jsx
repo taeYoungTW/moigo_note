d5e73828d5af3522a5805b662293970922e730c9
TaeYoung Kim <taezero@twinny.ai>
2021-11-16 17:51:55 +0900

    Mod: 연결된 컴포넌트 import 주소 수정, Scss 파일 import 추가

--- src/components/CreateCtrlBar/CreateCtrlBar.jsx
+++ src/components/CreateCtrlBar/CreateCtrlBar.jsx
@@ -1,10 +1,11 @@
 import React, { useCallback } from 'react';
-import AddBlockBtn from '../Common/AddBlockBtn';
-import ImgInput from '../Common/ImgInput';
+import AddBlockBtn from '../AddBlockBtn/AddBlockBtn';
+import ImgInput from '../ImgInput/ImgInput';
 import InsertPhotoIcon from '@mui/icons-material/InsertPhoto';
 import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
 import TextFieldsIcon from '@mui/icons-material/TextFields';
 import { useAppAction } from '../../contexts/AppStateContext';
+import './CreateCtrlBar.scss';
 
 const CreateCtrlBar = ({ handleSubmitBtnOnClick, submitBtnName, isUpdate }) => {
 	// Global States & Actions --------------------------
