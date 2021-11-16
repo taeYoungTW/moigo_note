5fbe9f8649cd7af01031e8dd8ab6e6aad1f386f0
TaeYoung Kim <taezero@twinny.ai>
2021-11-16 17:48:58 +0900

    Mod: 독립된 폴더로 이동

---
+++ src/components/CreateTextBlock/CreateTextBlock.jsx
@@ -0,0 +1,56 @@
+import { useCallback, useRef } from 'react';
+import './CreateTextBlock.scss';
+import { useAppAction } from '../../contexts/AppStateContext';
+import PropTypes from 'prop-types';
+import { WRITE_NOTE_TEXT } from '../../constants/constants';
+import useAutoHeightTextarea from '../../hooks/useAutoHeightTextarea';
+
+const CreateTextBlock = ({ block }) => {
+	// Global States, Actions ---------------------------------------
+	const { _updateBlock } = useAppAction();
+
+	// Local States ------------------------------------------------
+	// const [textBlock, setTextBlock] = useState(block);
+
+	// Ref --------------------------------------------------------
+	const textRef = useRef(null);
+
+	// Event Handler ----------------------------------------------
+	const handleTextOnChange = useCallback(
+		(e) => {
+			const {
+				target: { value },
+			} = e;
+			_updateBlock({ ...block, text: value });
+			// setTextBlock((block) => {
+			// 	return { ...block, text: value };
+			// });
+		},
+		[_updateBlock, block]
+	);
+
+	// hook : textarea auto height -----------------------------
+	useAutoHeightTextarea(textRef, block.text);
+
+	// Render -------------------------------------------------------
+	return (
+		<textarea
+			className="text_block_textarea"
+			type="text"
+			value={block.text}
+			onChange={handleTextOnChange}
+			placeholder={WRITE_NOTE_TEXT}
+			rows={1}
+			ref={textRef}
+			spellCheck={false}
+			autoFocus={true}
+		/>
+	);
+};
+
+// PropTypes ----------------------------------------------------------
+CreateTextBlock.propTypes = {
+	block: PropTypes.object,
+};
+
+export default CreateTextBlock;
---
+++ src/components/CtrlWindowBtn/CtrlWindowBtn.jsx
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types';
+import { CTRL_WINDOW_BTN_ICON_STYLE } from '../../constants/iconStyles';
+import './CtrlWindowBtn.scss';
+
+const CtrlWindowBtn = ({ Icon }) => {
+	// Render ---------------------------------
+	return (
+		<button className="ctrl_window_btn">
+			<Icon sx={CTRL_WINDOW_BTN_ICON_STYLE} className="ctrl_window_btn_icon" />
+		</button>
+	);
+};
+
+// PropTypes ---------------------------------
+CtrlWindowBtn.propTypes = {
+	Icon: PropTypes.elementType,
+};
+
+export default CtrlWindowBtn;
