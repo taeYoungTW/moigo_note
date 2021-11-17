8c0976f6f0689add1c5c9821339425ad04f11d92
TaeYoung Kim <taezero@twinny.ai>
2021-11-17 13:18:10 +0900

    Mod: 단순 blocks 정보를 map으로 돌려 ReadBlock 컴포넌트로 연결시키는 로직으로 단순화 시켰습니다.

--- src/components/ReadContent/ReadContent.jsx
+++ src/components/ReadContent/ReadContent.jsx
@@ -1,95 +1,16 @@
-import { INVALID_BLOCK_TYPE_TEXT } from '../../constants/constants';
-import useError from '../../hooks/useError';
-import ReadChecklistBlock from '../ReadBlocks/ReadChecklistBlock';
-import ReadImgBlock from '../ReadBlocks/ReadImgBlock';
-import ReadTextBlock from '../ReadBlocks/ReadTextBlock';
-import React, { useEffect, useState } from 'react';
-import reactDom from 'react-dom';
-/* 
-썸네일 이미지 컴포넌트
-*/
-const ReadContent = ({ note, isDetailNote }) => {
-	const _setUseError = useError();
+import ReadBlock from '../ReadBlocks/ReadBlock';
 
-	const summaryNoteImagesRoot = document.querySelector(
-		`#summary_note_images_root_${note.id}`
-	);
-
-	const [hasMounted, setHasMounted] = useState(false);
-	useEffect(() => {
-		setHasMounted(true);
-	}, []);
-
-	if (!hasMounted) {
-		return <></>;
-	}
-
-	const blockRouter = () => {
-		let isFirstImg = true;
-		return note?.blocks?.map((block) => {
-			switch (block.type) {
-				case 'text':
-					return (
-						<ReadTextBlock
-							block={block}
-							key={block.id}
-							isDetailNote={isDetailNote}
-						/>
-					);
-				case 'checklist':
-					return (
-						<ReadChecklistBlock
-							block={block}
-							key={block.id}
-							noteId={note.id}
-							isDetailNote={isDetailNote}
-						/>
-					);
-				case 'image':
-					if (isDetailNote) {
-						return (
-							<ReadImgBlock
-								block={block}
-								key={block.id}
-								isDetailNote={isDetailNote}
-							/>
-						);
-					}
-					if (isFirstImg) {
-						isFirstImg = false;
-						return reactDom.createPortal(
-							<img
-								src={block.dataUrl}
-								alt="summary_note_img"
-								className="summary_note_img"
-								key={block.id}
-							/>,
-							summaryNoteImagesRoot
-						);
-					} else {
-						return reactDom.createPortal(
-							<img
-								src={block.dataUrl}
-								alt="summary_note_img"
-								key={block.id}
-								style={{ display: 'none' }}
-							/>,
-							summaryNoteImagesRoot
-						);
-					}
-				default:
-					_setUseError({
-						message: INVALID_BLOCK_TYPE_TEXT,
-						location: 'SummaryNote/summary_area_el/switch',
-					});
-					return <></>;
-			}
-		});
-	};
-
+const ReadContent = ({ noteId, blocks, isDetailNote }) => {
 	return (
 		<div className={`${isDetailNote ? 'detailNote_' : ''}content`}>
-			{blockRouter()}
+			{blocks.map((block) => (
+				<ReadBlock
+					key={block.id}
+					block={block}
+					noteId={noteId}
+					isDetailNote={isDetailNote}
+				/>
+			))}
 		</div>
 	);
 };
