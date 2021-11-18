151c6da0ba1d29c4e63c8506776be65cf226ad4c
TaeYoung Kim <taezero@twinny.ai>
2021-11-18 14:06:17 +0900

    Mod: portal 방식으로 수정
    - 기존 global state인 _modalNote를 사용하지 않습니다.
    - summaryNote에서 note 정보를 prop으로 받습니다.
    - 모달을 닫는 로직 수행을 위해 setIsModalOn local setState를 전달 받아 내립니다.
    - summaryNote 클릭에 대한 버블링 방지 코드 추가

--- src/components/ModalNote/ModalNote.jsx
+++ src/components/ModalNote/ModalNote.jsx
@@ -2,16 +2,32 @@ import { useState } from 'react';
 import UpdateNote from '../UpdateNote/UpdateNote';
 import DetailNote from '../DetailNote/DetailNote';
 import './ModalNote.scss';
+import ReactDOM from 'react-dom';
 
-const ModalNote = () => {
+const ModalNote = ({ note, setIsModalOn }) => {
 	// Local States ------------------------------------------------
 	const [isEdit, setIsEdit] = useState(false);
+	const modalRoot = document.querySelector('#modal-root');
 
 	// Render -------------------------------------------------------
-	return (
-		<div className="modal_note">
-			{isEdit ? <UpdateNote /> : <DetailNote setIsEdit={setIsEdit} />}
-		</div>
+	return ReactDOM.createPortal(
+		<div
+			className="modal_note"
+			onClick={(e) => {
+				e.stopPropagation();
+			}}
+		>
+			{isEdit ? (
+				<UpdateNote note={note} setIsModalOn={setIsModalOn} />
+			) : (
+				<DetailNote
+					note={note}
+					setIsEdit={setIsEdit}
+					setIsModalOn={setIsModalOn}
+				/>
+			)}
+		</div>,
+		modalRoot
 	);
 };
 
