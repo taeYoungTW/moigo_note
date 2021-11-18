04a2395016840e30d6ac3b7cf1256f0811487671
TaeYoung Kim <taezero@twinny.ai>
2021-11-18 13:58:57 +0900

    Mod: global : _modalNote -> local :  note 수정
    - global state에서 local state를 prop으로 전달 받는 형식으로 변경
    - global state 사용시 note 정보 수정 할 때 마다 여러개의 global state를 연동 시켜주어야 했음
    - 1개의 global state 만을 받아 사용하는 방식으로 변경함 (_allNotes)

--- src/components/DetailNote/DetailNote.jsx
+++ src/components/DetailNote/DetailNote.jsx
@@ -1,32 +1,31 @@
 import './DetailNote.scss';
 import CloseIcon from '@mui/icons-material/Close';
-import { useAppAction, useAppState } from '../../contexts/AppStateContext';
+import { useAppAction } from '../../contexts/AppStateContext';
 import { useCallback, useState } from 'react';
 import {
 	DO_YOU_WANT_TO_DELETE_SELECTED_NOTES_TEXT,
 	EDIT_TEXT,
 } from '../../constants/constants';
-import ReadContent from '../ReadContent/ReadContent';
-import DeleteBtn from '../Common/DeleteBtn';
+import ReadBlocks from '../ReadBlocks/ReadBlocks';
+import DeleteBtn from '../DeleteBtn/DeleteBtn';
 import {
 	DETAIL_NOTE_DELETE_ICON_STYLE,
 	MODAL_NOTE_CLOSE_ICON_STYLE,
 } from '../../constants/iconStyles';
 import PortalConfirm from '../PortalConfirm/PortalConfirm';
 
-const DetailNote = ({ setIsEdit }) => {
+const DetailNote = ({ setIsEdit, note, setIsModalOn }) => {
 	// Global States, Actions ---------------------------------------
-	const { _modalNote } = useAppState();
-	const { _resetModalNote, _deleteNote, _setIsCreateNoteFormOn } =
-		useAppAction();
+	const { _deleteNote, _setIsCreateNoteFormOn } = useAppAction();
 
 	// Hooks ---------------------------------
 
 	// Local States ------------------------------------------------
-	const [isConfirmOn, setIsConfrimOn] = useState(false);
+	const [isConfirmOn, setIsConfirmOn] = useState(false);
+
 	// Event Handler ----------------------------------------------
 	const handleDeleteBtnOnClick = useCallback(() => {
-		setIsConfrimOn(true);
+		setIsConfirmOn(true);
 	}, []);
 
 	const handleSetEditBtnOnClick = useCallback(() => {
@@ -34,10 +33,13 @@ const DetailNote = ({ setIsEdit }) => {
 		_setIsCreateNoteFormOn(false); // _blocks를 같이 쓰기 때문에, 수정시 CreateNoteForm을 닫아 연동하여 표시될 _blocks를 막습니다.
 	}, [setIsEdit, _setIsCreateNoteFormOn]);
 
+	const handleCloseBtnOnClick = useCallback(() => {
+		setIsModalOn(false);
+	}, [setIsModalOn]);
+
 	const confirmCallback = () => {
-		const id = _modalNote.id;
-		_deleteNote(id);
-		_resetModalNote();
+		const noteId = note.id;
+		_deleteNote(noteId);
 	};
 
 	// Render -------------------------------------------------------
@@ -45,16 +47,12 @@ const DetailNote = ({ setIsEdit }) => {
 		<>
 			<div className="detail_note">
 				<div className="title_container">
-					<h1 className="title">{_modalNote.title}</h1>
-					<button className="close_btn" onClick={_resetModalNote}>
+					<h1 className="title">{note.title}</h1>
+					<button className="close_btn" onClick={handleCloseBtnOnClick}>
 						<CloseIcon sx={MODAL_NOTE_CLOSE_ICON_STYLE} />
 					</button>
 				</div>
-				<ReadContent
-					blocks={_modalNote.blocks}
-					noteId={_modalNote.id}
-					isDetailNote={true}
-				/>
+				<ReadBlocks blocks={note.blocks} noteId={note.id} isDetailNote={true} />
 				<div className="ctrl_bar">
 					<DeleteBtn
 						className="delete_btn"
@@ -73,7 +71,7 @@ const DetailNote = ({ setIsEdit }) => {
 			<PortalConfirm
 				question={DO_YOU_WANT_TO_DELETE_SELECTED_NOTES_TEXT}
 				isConfirmOn={isConfirmOn}
-				setIsConfirmOn={setIsConfrimOn}
+				setIsConfirmOn={setIsConfirmOn}
 				confirmCallback={confirmCallback}
 			/>
 		</>
