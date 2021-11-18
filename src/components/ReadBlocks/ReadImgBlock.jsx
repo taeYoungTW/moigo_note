10d6c0c5f8ab1b474f88d3789baec174b168e3e5
TaeYoung Kim <taezero@twinny.ai>
2021-11-18 15:07:01 +0900

    Del: 위치 수정, 이름 변경 및 리팩토링에 의한 삭제

--- src/components/ReadBlocks/ReadImgBlock.jsx
+++
@@ -1,7 +0,0 @@
-const ReadImgBlock = ({ block, isDetailNote }) => {
-	return (
-		<img src={block.dataUrl} alt="read_img_block" className="read_img_block" />
-	);
-};
-
-export default ReadImgBlock;
