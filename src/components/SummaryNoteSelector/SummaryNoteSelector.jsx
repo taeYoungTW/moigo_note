2c762dbb48a2a079e9764271a326bb7a33c14a9f
TaeYoung Kim <taezero@twinny.ai>
2021-11-18 14:55:52 +0900

    Add: SummaryNote.scss 스타일 분할로 인한 스타일 생성
    - className kebab case 수정

---
+++ src/components/SummaryNoteSelector/SummaryNoteSelector.jsx
@@ -0,0 +1,61 @@
+import { useCallback, useEffect, useState } from 'react';
+import { useAppAction, useAppState } from '../../contexts/AppStateContext';
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import { SUMMARY_NOTE_CHECK_CIRCLE_ICON_STYLE } from '../../constants/iconStyles';
+
+const SummaryNoteSelector = ({ noteId }) => {
+	const { _selectedNoteIds } = useAppState();
+	const { _addSelectedNoteId, _deleteSelectedNoteId } = useAppAction();
+
+	const [isSelected, setIsSelected] = useState(
+		_selectedNoteIds.includes(noteId)
+	);
+
+	const handleSelectBtnOnClick = useCallback(() => {
+		setIsSelected(true);
+		_addSelectedNoteId(noteId);
+	}, [_addSelectedNoteId, noteId]);
+
+	const handleUnselectBtnOnClick = useCallback(() => {
+		setIsSelected(false);
+		_deleteSelectedNoteId(noteId);
+	}, [_deleteSelectedNoteId, noteId]);
+
+	useEffect(() => {
+		setIsSelected(_selectedNoteIds.includes(noteId));
+	}, [_selectedNoteIds, noteId]);
+
+	return (
+		<div
+			className="summary-note-selector"
+			onClick={(e) => {
+				e.stopPropagation();
+			}}
+		>
+			{isSelected ? (
+				<>
+					<button
+						className="now-select-on-btn"
+						onClick={handleUnselectBtnOnClick}
+					>
+						<CheckCircleIcon sx={SUMMARY_NOTE_CHECK_CIRCLE_ICON_STYLE} />
+					</button>
+					<div className="now-select-on-outline"></div>
+					{/*
+					 * outline을 border로 하지 않은 이유
+					 * - border를 두껍게 하는 경우 content box가 작아져서 내용 표시가 변화됩니다.
+					 * - select 되었을 경우 해당 요소를 클릭해도 detailNote가 떠서는 안됩니다.
+					 * - 요소를 앞에 두어 막음으로서 css 만으로 제어할 수 있습니다.
+					 */}
+				</>
+			) : (
+				<button className="now-select-off-btn" onClick={handleSelectBtnOnClick}>
+					<CheckCircleOutlineIcon sx={SUMMARY_NOTE_CHECK_CIRCLE_ICON_STYLE} />
+				</button>
+			)}
+		</div>
+	);
+};
+
+export default SummaryNoteSelector;
