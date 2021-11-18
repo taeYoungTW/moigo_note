1369f3cea881a8c3be1929e09e0acb1d13032989
TaeYoung Kim <taezero@twinny.ai>
2021-11-18 14:12:21 +0900

    Add: ReadCotent 로직 분할 -> ReadBlocks, ReadBlock
    - 기존 ReadContent 로직을 분할하고 이름도 내용에 충실하게 변경했습니다.
    - ReadContent -> ReadBlocks
    - ReadContent에서 Block 매칭을 담당하는 컴포넌트로 ReadBlock으로 이동하였습니다.

---
+++ src/components/ReadBlock/ReadBlock.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { INVALID_BLOCK_TYPE_TEXT } from '../../constants/constants';
+import useError from '../../hooks/useError';
+import ReadChecklistBlock from '../ReadChecklistBlock/ReadChecklistBlock';
+import ReadImgBlock from '../ReadImgBlock/ReadImgBlock';
+import ReadTextBlock from '../ReadTextBlock/ReadTextBlock';
+import './ReadBlock.scss';
+
+const ReadBlock = ({ block, noteId, isDetailNote, blockIndex }) => {
+	const _setUseError = useError();
+
+	const blockRouter = (blockType) => {
+		switch (blockType) {
+			case 'text':
+				return <ReadTextBlock block={block} key={block.id} />;
+			case 'checklist':
+				return (
+					<ReadChecklistBlock
+						block={block}
+						key={block.id}
+						noteId={noteId}
+						blockIndex={blockIndex}
+					/>
+				);
+			case 'image':
+				if (!isDetailNote) {
+					return <></>;
+				}
+				return <ReadImgBlock block={block} key={block.id} />;
+			default:
+				_setUseError({
+					message: INVALID_BLOCK_TYPE_TEXT,
+					location: 'DetailNote/ReadBlock/DetailBlockRouter',
+				});
+				return <></>;
+		}
+	};
+
+	return (
+		<div className={`read_block ${isDetailNote ? 'detail' : ''}`}>
+			{blockRouter(block.type)}
+		</div>
+	);
+};
+
+export default ReadBlock;
