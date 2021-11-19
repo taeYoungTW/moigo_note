f112357d5c33445b280fb76b0654ac42080c49c0
TaeYoung Kim <taezero@twinny.ai>
2021-11-19 17:22:02 +0900

    Mod: SummaryNoteContent에 존재하던 thumbnail 로직 내부로 위치
    - SummaryNoteContent 의 restBlocks 불필요로 인해 splitImgBlocks 관련 로직 모두 이동

--- src/components/SummaryNoteThumbnail/SummaryNoteThumbnail.jsx
+++ src/components/SummaryNoteThumbnail/SummaryNoteThumbnail.jsx
@@ -1,22 +1,50 @@
+import { useEffect, useState } from 'react';
 import './SummaryNoteThumbnail.scss';
 
-const SummaryNoteThumbnail = ({ thumbnail }) => {
+const SummaryNoteThumbnail = ({ blocks }) => {
+	// Local States -----------------------------------------------
+	const [thumbnail, setThumbnail] = useState({ dataUrl: '', count: 0 });
+
+	// Function -----------------------------------------------
+	const splitImgBlocks = (blocks) => {
+		let count = 0;
+		let firstImg = {};
+
+		for (let i = 0; i < blocks.length; i++) {
+			if (blocks[i].type === 'image') {
+				if (count === 0) {
+					firstImg = blocks[i].dataUrl;
+				}
+				count += 1;
+			}
+		}
+
+		setThumbnail({ dataUrl: firstImg, count });
+	};
+
+	// useEffect --------------------------------
+	useEffect(() => {
+		splitImgBlocks(blocks);
+	}, [blocks]);
+
 	return (
-		<div className="summary-note-thumbnail-container">
-			<img
-				className="summary-note-thumbnail"
-				src={thumbnail.block.dataUrl}
-				alt="summary-note-thumbnail"
-			/>
-			<div
-				className="summary-note-img-count"
-				style={{
-					display: thumbnail.count < 2 ? 'none' : '',
-				}}
-			>
-				{`${thumbnail.count - 1}+`}
+		thumbnail.count !== 0 && (
+			<div className="summary-note-thumbnail-container">
+				<img
+					className="summary-note-thumbnail"
+					src={thumbnail.dataUrl}
+					alt="summary-note-thumbnail"
+				/>
+				<div
+					className="summary-note-img-count"
+					style={{
+						display: thumbnail.count < 2 ? 'none' : '',
+					}}
+				>
+					{`${thumbnail.count - 1}+`}
+				</div>
 			</div>
-		</div>
+		)
 	);
 };
 
