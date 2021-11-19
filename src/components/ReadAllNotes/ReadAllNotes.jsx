6ac651985009f5b99e00006c77be2ec3ca0d51c9
TaeYoung Kim <taezero@twinny.ai>
2021-11-19 17:17:49 +0900

    Mod: moveNote prop 전달에서 DnDGridNoteHOC 내부로 변경
    - ReadAllNotes 컴포넌트 위치의 부적절성으로 인한 이동

--- src/components/ReadAllNotes/ReadAllNotes.jsx
+++ src/components/ReadAllNotes/ReadAllNotes.jsx
@@ -1,23 +1,12 @@
-import { useAppAction, useAppState } from '../../contexts/AppStateContext';
+import { useAppState } from '../../contexts/AppStateContext';
 import './ReadAllNotes.scss';
 import DnDGridNoteHOC from '../DnDGridNoteHOC/DnDGridNoteHOC';
-import { useCallback } from 'react';
 import SummaryNote from '../SummaryNote/SummaryNote';
 
 const ReadAllNotes = () => {
 	// Global States, Actions ---------------------------------------
 	const { _allNotes } = useAppState();
-	const { _setNotes } = useAppAction();
 
-	const moveNotes = useCallback(
-		(dragIndex, hoverIndex) => {
-			const newNotes = [..._allNotes];
-			const [draggedNote] = newNotes.splice(dragIndex, 1);
-			newNotes.splice(hoverIndex, 0, draggedNote);
-			_setNotes(newNotes);
-		},
-		[_allNotes, _setNotes]
-	);
 	// Render -----------------------------------------------
 	return (
 		<section className="read-all-notes">
@@ -26,7 +15,6 @@ const ReadAllNotes = () => {
 					key={note.id}
 					id={note.id}
 					index={i}
-					moveNotes={moveNotes}
 					Component={SummaryNote}
 					note={note}
 				/>
