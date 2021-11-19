2a0555e1f4650f1741fb04dee2f771305ab17940
TaeYoung Kim <taezero@twinny.ai>
2021-11-19 17:19:24 +0900

    Mod: splitImgBlocks 및 thumbnail관련 함수 thumbnail로 이동
    - restBlocks의 불필요로 인해 위치 개편

--- src/components/SummaryNoteContent/SummaryNoteContent.jsx
+++ src/components/SummaryNoteContent/SummaryNoteContent.jsx
@@ -1,42 +1,11 @@
-import React, { useEffect, useState } from 'react';
 import ReadBlocks from '../ReadBlocks/ReadBlocks';
 import SummaryNoteThumbnail from '../SummaryNoteThumbnail/SummaryNoteThumbnail';
 import './SummaryNoteContent.scss';
 
 const SummaryNoteContent = ({ note }) => {
-	// Local States -----------------------------------------------
-	const [thumbnail, setThumbnail] = useState({ block: {}, count: 0 });
-	const [restBlocks, setRestBlocks] = useState([]);
-
-	// Function -----------------------------------------------
-	const splitImgBlocks = (blocks) => {
-		let count = 0;
-		let firstImg = {};
-
-		const restBlocks = blocks.filter((block) => {
-			if (block.type === 'image') {
-				if (count === 0) {
-					firstImg = block;
-				}
-				count += 1;
-				return false;
-			} else {
-				return true;
-			}
-		});
-
-		setThumbnail({ block: firstImg, count });
-		setRestBlocks(restBlocks);
-	};
-
-	// useEffect --------------------------------
-	useEffect(() => {
-		splitImgBlocks(note.blocks);
-	}, [note]);
-
 	return (
 		<div className="summary-note-content">
-			{thumbnail.count !== 0 && <SummaryNoteThumbnail thumbnail={thumbnail} />}
+			<SummaryNoteThumbnail blocks={note.blocks} />
 			<div className="summary-note-title-blocks">
 				{note.title && <h1 className="summary-note-title">{note.title}</h1>}
 				<ReadBlocks blocks={note.blocks} noteId={note.id} isSummaryNote />
