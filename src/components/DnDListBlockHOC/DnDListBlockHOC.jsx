15d2f6cfd377ae1e9e17bc1f623a353af3cfd50a
TaeYoung Kim <taezero@twinny.ai>
2021-11-24 14:16:04 +0900

    Mod: indicator -> dragLayout으로 blockReorder 구현
    - dragLayout 적용을 위해 block 정보를 item에 전달
    - 높이가 다른 block간의 Reorder 문제를 if문을 통해서 통제

--- src/components/DnDListBlockHOC/DnDListBlockHOC.jsx
+++ src/components/DnDListBlockHOC/DnDListBlockHOC.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { useDrag, useDrop } from 'react-dnd';
 import { useAppAction, useAppState } from '../../contexts/AppStateContext';
 
@@ -6,15 +6,14 @@ import { ItemTypes } from '../../constants/constants';
 
 import './DnDListBlockHOC.scss';
 import BlockCtrlBtns from '../BlockCtrlBtns/BlockCtrlBtns';
+import { getEmptyImage } from 'react-dnd-html5-backend';
 
-const DnDListBlockHOC = ({ blockId, blockIndex, Component, ComponentProp }) => {
+const DnDListBlockHOC = ({ block, blockIndex, Component, ComponentProp }) => {
 	// Global States & Actions
 	const { _blocks } = useAppState();
 	const { _initBlocks } = useAppAction();
 
-	// Local States
-	const [isIndexSame, setIsIndexSame] = useState(false);
-	const [isBottomDrag, setIsBottomDrag] = useState(false);
+	const currentRef = useRef(null);
 
 	// Functions
 	const moveBlock = useCallback(
@@ -28,57 +27,72 @@ const DnDListBlockHOC = ({ blockId, blockIndex, Component, ComponentProp }) => {
 	);
 
 	// React-Dnd Hooks
-	const [{ isOver }, drop] = useDrop({
+	const [, drop] = useDrop({
 		accept: ItemTypes.BLOCK,
 		collect: (monitor) => {
 			return { isOver: monitor.isOver() };
 		},
-		hover(dragItem) {
+		hover(dragItem, monitor) {
 			const { dragIndex } = dragItem;
-			setIsIndexSame(false);
-			setIsBottomDrag(false);
 
 			if (dragIndex === blockIndex) {
-				setIsIndexSame(true);
 				return;
 			}
 
-			if (dragIndex > blockIndex) {
-				setIsBottomDrag(true);
+			if (!currentRef.current) {
+				return;
 			}
-		},
-		drop(dragItem) {
-			const { dragIndex } = dragItem;
+
+			const hoverBoundingRect = currentRef.current?.getBoundingClientRect();
+
+			const { bottom, top } = hoverBoundingRect;
+			const dropMiddleY = top + (bottom - top) / 2;
+			const dragClientY = monitor.getClientOffset().y;
+
+			// 위에서 아래로 이동하는 경우
+			if (dragIndex < blockIndex) {
+				if (dragClientY < dropMiddleY) {
+					return;
+				}
+			}
+
+			// 아래에서 위로 이동하는 경우
+			if (dragIndex > blockIndex) {
+				if (dragClientY > dropMiddleY) {
+					return;
+				}
+			}
 
 			moveBlock(dragIndex, blockIndex);
 			dragItem.dragIndex = blockIndex;
 		},
 	});
 
-	const [, drag, preview] = useDrag({
+	const [{ isDragging }, drag, preview] = useDrag({
 		type: ItemTypes.BLOCK,
-		item: () => ({ dragId: blockId, dragIndex: blockIndex }),
+		item: () => ({
+			dragIndex: blockIndex,
+			block,
+			blockHeight: currentRef?.current?.clientHeight,
+		}),
+		collect: (monitor) => ({
+			isDragging: monitor.isDragging(),
+		}),
 	});
 
+	preview(getEmptyImage());
 	return (
 		<div ref={drop}>
 			<div
-				className="block-location-indicator"
 				style={{
-					display: isOver && !isIndexSame && isBottomDrag ? '' : 'none',
+					opacity: isDragging ? 0 : 1,
 				}}
-			></div>
-			<div ref={preview}>
+				ref={currentRef}
+			>
 				<Component {...ComponentProp}>
 					<BlockCtrlBtns blockId={ComponentProp.block.id} ref={drag} />
 				</Component>
 			</div>
-			<div
-				className="block-location-indicator"
-				style={{
-					display: isOver && !isIndexSame && !isBottomDrag ? '' : 'none',
-				}}
-			></div>
 		</div>
 	);
 };
