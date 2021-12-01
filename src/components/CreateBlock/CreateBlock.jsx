3929f9d55fa1dcdb1018dde3b30781098f7537ee
TaeYoung Kim <taezero@twinny.ai>
2021-12-01 15:20:44 +0900

    Mod: forwardRef 방식으로 변경하여 dropRef를 받음

--- src/components/CreateBlock/CreateBlock.jsx
+++ src/components/CreateBlock/CreateBlock.jsx
@@ -1,3 +1,4 @@
+import { forwardRef } from 'react';
 import { INVALID_BLOCK_TYPE_TEXT } from '../../constants/constants';
 import useError from '../../hooks/useError';
 import CreateChecklistBlock from '../CreateChecklistBlock/CreateChecklistBlock';
@@ -5,35 +6,43 @@ import CreateImgBlock from '../CreateImgBlock/CreateImgBlock';
 import CreateTextBlock from '../CreateTextBlock/CreateTextBlock';
 import './CreateBlock.scss';
 
-const CreateBlock = ({ block, children, blockIndex }) => {
-	// Hook ----------------------------
-	const _setUseError = useError();
+const CreateBlock = forwardRef(
+	({ block, children, blockIndex, isDragging }, dropRef) => {
+		// Hook ----------------------------
+		const _setUseError = useError();
 
-	// Functions ----------------------------
-	const blockRouter = (BlockType) => {
-		switch (BlockType) {
-			case 'text':
-				return <CreateTextBlock block={block} />;
-			case 'checklist':
-				return <CreateChecklistBlock block={block} blockIndex={blockIndex} />;
-			case 'image':
-				return <CreateImgBlock block={block} />;
-			default:
-				_setUseError({
-					message: INVALID_BLOCK_TYPE_TEXT,
-					location: 'CreateBlock',
-				});
-				return <></>;
-		}
-	};
+		// Functions ----------------------------
+		const blockRouter = (BlockType) => {
+			switch (BlockType) {
+				case 'text':
+					return <CreateTextBlock block={block} />;
+				case 'checklist':
+					return <CreateChecklistBlock block={block} blockIndex={blockIndex} />;
+				case 'image':
+					return <CreateImgBlock block={block} />;
+				default:
+					_setUseError({
+						message: INVALID_BLOCK_TYPE_TEXT,
+						location: 'CreateBlock',
+					});
+					return <></>;
+			}
+		};
 
-	// render ----------------------------
-	return (
-		<div className="create-block">
-			{blockRouter(block.type)}
-			{children}
-		</div>
-	);
-};
+		// render ----------------------------
+		return (
+			<div
+				className="create-block"
+				ref={dropRef}
+				style={{
+					opacity: isDragging ? 0 : 1,
+				}}
+			>
+				{blockRouter(block.type)}
+				{children}
+			</div>
+		);
+	}
+);
 
 export default CreateBlock;
