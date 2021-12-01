a42574e1a8debb8c144d78cc40012fdab40b9f3c
TaeYoung Kim <taezero@twinny.ai>
2021-12-01 15:59:30 +0900

    Add: DnDListBlockHOC을 hook으로 만듦
    - Component 흐름을 읽기 쉽게 하기 위해서 변경하였습니다.

---
+++ src/hooks/useDnDListBlock.jsx
@@ -0,0 +1,59 @@
+import { useRef } from 'react';
+import { useDrag, useDrop } from 'react-dnd';
+import { getEmptyImage } from 'react-dnd-html5-backend';
+import { ItemTypes } from '../constants/constants';
+
+const useDnDListBlock = (index, block, moveBlock) => {
+	const currentRef = useRef(null);
+
+	// React-Dnd Hooks
+	const [, drop] = useDrop({
+		accept: ItemTypes.BLOCK,
+		collect: (monitor) => {
+			return { isOver: monitor.isOver() };
+		},
+		hover(dragItem, monitor) {
+			const { dragIndex, blockHeight } = dragItem;
+
+			if (dragIndex === index) {
+				return;
+			}
+
+			if (!currentRef.current) {
+				return;
+			}
+
+			const hoverBoundingRect = currentRef.current?.getBoundingClientRect();
+
+			const { bottom: dropBottom } = hoverBoundingRect;
+			const cursorClientY = monitor.getClientOffset().y;
+
+			// 위에서 아래로 이동하는 경우
+			if (dragIndex < index) {
+				if (cursorClientY < dropBottom - blockHeight) {
+					return;
+				}
+			}
+
+			moveBlock(dragIndex, index);
+			dragItem.dragIndex = index;
+		},
+	});
+
+	const [{ isDragging }, drag, preview] = useDrag({
+		type: ItemTypes.BLOCK,
+		item: () => ({
+			dragIndex: index,
+			block,
+			blockHeight: currentRef?.current?.clientHeight,
+		}),
+		collect: (monitor) => ({
+			isDragging: monitor.isDragging(),
+		}),
+	});
+
+	preview(getEmptyImage());
+	return { dropRef: drop(currentRef), dragRef: drag, isDragging };
+};
+
+export default useDnDListBlock;
