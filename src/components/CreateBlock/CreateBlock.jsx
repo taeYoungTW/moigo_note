4c3a4fee5c02d7e555dddd574e46c85fe95490a9
TaeYoung Kim <taezero@twinny.ai>
2021-12-01 15:54:06 +0900

    Mod: useDnDListBlock Hook 연결
    - 기존 DnDListBlockHOC 방식에서 hook 방식으로 변경

--- src/components/CreateBlock/CreateBlock.jsx
+++ src/components/CreateBlock/CreateBlock.jsx
@@ -1,48 +1,69 @@
-import { forwardRef } from 'react';
+import { useCallback } from 'react';
 import { INVALID_BLOCK_TYPE_TEXT } from '../../constants/constants';
+import { useAppAction, useAppState } from '../../contexts/AppStateContext';
+import useDnDListBlock from '../../hooks/useDnDListBlock';
 import useError from '../../hooks/useError';
+import BlockCtrlBtns from '../BlockCtrlBtns/BlockCtrlBtns';
 import CreateChecklistBlock from '../CreateChecklistBlock/CreateChecklistBlock';
 import CreateImgBlock from '../CreateImgBlock/CreateImgBlock';
 import CreateTextBlock from '../CreateTextBlock/CreateTextBlock';
 import './CreateBlock.scss';
 
-const CreateBlock = forwardRef(
-	({ block, children, blockIndex, isDragging }, dropRef) => {
-		// Hook ----------------------------
-		const _setUseError = useError();
+const CreateBlock = ({ block, index }) => {
+	// Global States & Actions ---------------------------------------
+	const { _blocks } = useAppState();
+	const { _initBlocks } = useAppAction();
 
-		// Functions ----------------------------
-		const blockRouter = (BlockType) => {
-			switch (BlockType) {
-				case 'text':
-					return <CreateTextBlock block={block} />;
-				case 'checklist':
-					return <CreateChecklistBlock block={block} blockIndex={blockIndex} />;
-				case 'image':
-					return <CreateImgBlock block={block} />;
-				default:
-					_setUseError({
-						message: INVALID_BLOCK_TYPE_TEXT,
-						location: 'CreateBlock',
-					});
-					return <></>;
-			}
-		};
+	// Functions ---------------------------------------
+	const moveBlock = useCallback(
+		(dragIndex, hoverIndex) => {
+			const newBlocks = [..._blocks];
+			const [draggedBlock] = newBlocks.splice(dragIndex, 1);
+			newBlocks.splice(hoverIndex, 0, draggedBlock);
+			_initBlocks(newBlocks);
+		},
+		[_blocks, _initBlocks]
+	);
 
-		// render ----------------------------
-		return (
-			<div
-				className="create-block"
-				ref={dropRef}
-				style={{
-					opacity: isDragging ? 0 : 1,
-				}}
-			>
-				{blockRouter(block.type)}
-				{children}
-			</div>
-		);
-	}
-);
+	const blockRouter = (BlockType) => {
+		switch (BlockType) {
+			case 'text':
+				return <CreateTextBlock block={block} />;
+			case 'checklist':
+				return <CreateChecklistBlock block={block} blockIndex={index} />;
+			case 'image':
+				return <CreateImgBlock block={block} />;
+			default:
+				_setUseError({
+					message: INVALID_BLOCK_TYPE_TEXT,
+					location: 'CreateBlock',
+				});
+				return <></>;
+		}
+	};
+
+	/* Hooks ---------------------------- */
+	const _setUseError = useError();
+
+	const { dropRef, dragRef, isDragging } = useDnDListBlock(
+		index,
+		block,
+		moveBlock
+	);
+
+	// render ----------------------------
+	return (
+		<div
+			className="create-block"
+			ref={dropRef}
+			style={{
+				opacity: isDragging ? 0 : 1,
+			}}
+		>
+			{blockRouter(block.type)}
+			<BlockCtrlBtns blockId={block.id} ref={dragRef} />
+		</div>
+	);
+};
 
 export default CreateBlock;
