4af1f5048726c2b066f2509c042e7ad19a3f38f1
TaeYoung Kim <taezero@twinny.ai>
2021-12-08 13:59:16 +0900

    Mod: 이미지 블록 포커스 및 블록삭제, 포커스 이동 등.. 단축키 구현
    - Focus가 가능한 요소로 만들기 -> tabIndex={0}
    - useIsPrevBlockToFocus hook을 변형하여 img에서만 적용할 useEffect 만들기
      - For focusing with ArrowKey 주석 부분 참고
    - 이미지 블록을 만들어 내는 경우 스크롤 이동 및 focus 시키는 기능 추가
      - For focusing when it was mouted at first 주석 부분 참고
    - onKeyDown
     -> Enter : textblock 아래 추가
     -> Backspace : 상위 블록 포커스 & 해당 이미지 블록 삭제
     -> ArrowKey: 상위, 하위 블록 포커스 이동
    - onBlur : _indesToFocus 값 초기화

--- src/components/CreateImgBlock/CreateImgBlock.jsx
+++ src/components/CreateImgBlock/CreateImgBlock.jsx
@@ -1,7 +1,75 @@
+import { useEffect, useRef } from 'react';
+import { BlockTypes } from '../../constants/constants';
+import { useAppAction, useAppState } from '../../contexts/AppStateContext';
+import {
+	handleBlockWithArrowKey,
+	handleBlockWithBackspaceKey,
+	handleBlockWithEnterKey,
+} from '../../utils/handleBlockOnkeyDown';
 import styles from './CreateImgBlock.scss';
 
-const CreateImgBlock = ({ block }) => {
-	return <img src={block.dataUrl} alt="img_block" className={styles.img} />;
+const CreateImgBlock = ({ block, blockIndex }) => {
+	/* ---- Global States & Actions ------------------------------ */
+	const { _indexToFocus } = useAppState();
+	const { _deleteBlock, _setIndexToFocus, _addTypeBlock } = useAppAction();
+
+	/* ---- Ref -------------------------------------------------- */
+	const imgRef = useRef(null);
+
+	/* ---- EventHandlers ---------------------------------------- */
+	// onKeyDown
+	const handleOnKeyDown = (e) => {
+		handleBlockWithEnterKey(e, () => {
+			_addTypeBlock(BlockTypes.TEXT, undefined, blockIndex + 1);
+		});
+
+		handleBlockWithBackspaceKey(e, '', () => {
+			_setIndexToFocus(blockIndex - 1);
+			_deleteBlock(block.id);
+		});
+
+		handleBlockWithArrowKey(
+			e,
+			() => _setIndexToFocus(blockIndex - 1),
+			() => _setIndexToFocus(blockIndex + 1),
+			true
+		);
+	};
+
+	// onBlur
+	// Clean Up (For Checking a Change in useIsPrevBlockToFocus hook or like useEffect)
+	const handleOnBlur = () => {
+		if (_indexToFocus === -1) {
+			return;
+		}
+		_setIndexToFocus(-1);
+	};
+
+	/* ---- useEffects ---------------------------------------- */
+	// For focusing with ArrowKey
+	useEffect(() => {
+		if (blockIndex === _indexToFocus) {
+			imgRef.current.focus();
+		}
+	}, [blockIndex, _indexToFocus]);
+
+	// For focusing when it was mounted at first
+	useEffect(() => {
+		imgRef.current.focus();
+	}, []);
+
+	/* ---- Render ---------------------------------------- */
+	return (
+		<img
+			tabIndex={0}
+			ref={imgRef}
+			src={block.dataUrl}
+			alt="img_block"
+			className={styles.img}
+			onKeyDown={handleOnKeyDown}
+			onBlur={handleOnBlur}
+		/>
+	);
 };
 
 export default CreateImgBlock;
