7a97a62d28bea7cf3d5f0fc9ccdc15dbdfe73822
TaeYoung Kim <taezero@twinny.ai>
2021-12-13 18:20:02 +0900

    Del: custom hook으로 변경되어 제거

--- src/utils/setCaretEnd.js
+++
@@ -1,7 +0,0 @@
-const setCaretEnd = (targetDOM) => {
-	// For #4 issue
-	targetDOM.focus();
-	targetDOM.setSelectionRange(targetDOM.value.length, targetDOM.value.length);
-};
-
-export default setCaretEnd;
