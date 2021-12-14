e448330dbce5788facac7f6166c0e9ace7c52b85
TaeYoung Kim <taezero@twinny.ai>
2021-12-15 08:38:04 +0900

    Del: dev1_eject_reducer Branch 백업으로 인한 삭제

--- src/contextStore/block/types.js
+++
@@ -1,7 +0,0 @@
-// Types
-export const RESET_BLOCKS = 'block/resetBlocks';
-export const SYNC_BLOCKS = 'block/syncBlocks';
-export const ADD_BLOCK = 'block/addBlock';
-export const DELETE_BLOCK = 'block/deleteBlock';
-export const UPDATE_BLOCK = 'block/updateBlock';
-export const MOVE_BLOCK_TO_END = 'block/moveBlockToEnd';
