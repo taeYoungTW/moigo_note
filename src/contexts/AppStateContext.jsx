62a84210d64ac647e06b4f9eb9d04d2259196fcd
TaeYoung Kim <taezero@twinny.ai>
2021-12-15 08:38:39 +0900

    Mod: reducer 사용 방식 전 방식으로 되돌림

--- src/contexts/AppStateContext.jsx
+++ src/contexts/AppStateContext.jsx
@@ -1,8 +1,7 @@
-import { createContext, useContext, useReducer } from 'react';
+import { createContext, useContext } from 'react';
 import Note from '../contextStore/Note';
 import Block from '../contextStore/Block';
 import Proptypes from 'prop-types';
-import block from '../contextStore/block/reducer';
 
 const AppStateContext = createContext();
 // State와 Action을 구분하여 사용하기 위함
@@ -11,22 +10,6 @@ const useAppAction = () => useContext(AppStateContext);
 
 // Provider
 const AppStateProvider = ({ children }) => {
-	// Combine Reducer & Initial state
-	const combineReducers = (reducersObj) => {
-		return (state, action) => {
-			const keys = Object.keys(reducersObj);
-			for (let i = 0; i < keys.length; i++) {
-				reducersObj[keys[i]](state, action);
-			}
-		};
-	};
-
-	const reducer = combineReducers({ block });
-	const combineInitialState = { ...block() };
-
-	// Root useReducer
-	const [state, dispatch] = useReducer(reducer, combineInitialState);
-
 	// Combine
 	const combine = { ...Note(), ...Block() };
 
