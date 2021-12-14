94113da4a4d6a1b241bcd2a93c02f9e276183e5b
TaeYoung Kim <taezero@twinny.ai>
2021-12-14 17:50:11 +0900

    Mod: useReducer 방식 사용을 위한 설정
    - combineReducers

--- src/contexts/AppStateContext.jsx
+++ src/contexts/AppStateContext.jsx
@@ -1,7 +1,8 @@
-import { createContext, useContext } from 'react';
+import { createContext, useContext, useReducer } from 'react';
 import Note from '../contextStore/Note';
 import Block from '../contextStore/Block';
 import Proptypes from 'prop-types';
+import block from '../contextStore/block/reducer';
 
 const AppStateContext = createContext();
 // State와 Action을 구분하여 사용하기 위함
@@ -10,6 +11,22 @@ const useAppAction = () => useContext(AppStateContext);
 
 // Provider
 const AppStateProvider = ({ children }) => {
+	// Combine Reducer & Initial state
+	const combineReducers = (reducersObj) => {
+		return (state, action) => {
+			const keys = Object.keys(reducersObj);
+			for (let i = 0; i < keys.length; i++) {
+				reducersObj[keys[i]](state, action);
+			}
+		};
+	};
+
+	const reducer = combineReducers({ block });
+	const combineInitialState = { ...block() };
+
+	// Root useReducer
+	const [state, dispatch] = useReducer(reducer, combineInitialState);
+
 	// Combine
 	const combine = { ...Note(), ...Block() };
 
