0b76664c6cdd85d7d9bdbb63eeeda48e13d918ed
TaeYoung Kim <taezero@twinny.ai>
2021-12-15 10:13:49 +0900

    Mod: useDnDGridNote Hook 수정 반영
    -  기존 개별 Global action&state 를 받아 처리하는 방식에서
    -> useAppAction을 받아 Hook 내부에서 처리하도록 변경
    
    - useDnDGridNote Hook
    :moveNote -> useAppAction
    - moveNote 함수 로직을 hook 내부로 이전

--- src/components/SummaryNote/SummaryNote.jsx
+++ src/components/SummaryNote/SummaryNote.jsx
@@ -4,40 +4,28 @@ import PropTypes from 'prop-types';
 import useSearch from '../../hooks/useSearch';
 import SummaryNoteContent from '../SummaryNoteContent/SummaryNoteContent';
 import SummaryNoteCtrl from '../SummaryNoteCtrl/SummaryNoteCtrl';
-import { useCallback, useState } from 'react';
+import { useState } from 'react';
 import ModalNote from '../ModalNote/ModalNote';
 import useDnDGridNote from '../../hooks/useDnDGridNote';
 
 const SummaryNote = ({ note, index }) => {
-	// Global States & Actions --------------
-	const { _searchInput, _allNotes } = useAppState();
-	const { _setNotes } = useAppAction();
+	/* ---- Global States & Actions ------------------------------ */
+	const { _searchInput } = useAppState();
 
-	// Function for useDnDGridNote ----------------------
-	const moveNote = useCallback(
-		(dragIndex, hoverIndex) => {
-			const newNotes = [..._allNotes];
-			const [draggedNote] = newNotes.splice(dragIndex, 1);
-			newNotes.splice(hoverIndex, 0, draggedNote);
-			_setNotes(newNotes);
-		},
-		[_allNotes, _setNotes]
-	);
-
-	// Hook --------------------------------------
+	/* ---- Hooks ------------------------------------------------ */
 	const isDisplay = useSearch(_searchInput, note);
-	const { dndRef, isDragging } = useDnDGridNote(index, note, moveNote);
 
-	// Local State
+	const { dndRef, isDragging } = useDnDGridNote(index, note, useAppAction);
+
+	/* ---- Local States --------------------------------------- */
 	const [isModalOn, setIsModalOn] = useState(false);
 
-	/* ---- Event Handler ------------------------------ */
-
+	/* ---- Event Handler --------------------------------------- */
 	const handleSummaryNoteClick = () => {
 		setIsModalOn(true);
 	};
 
-	// Render -------------------------------------------
+	/* ---- Render ---------------------------------------------- */
 	return (
 		<article
 			className={styles.summaryNote}
